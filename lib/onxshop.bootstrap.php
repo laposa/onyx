c81e2916b882e48c96db81cf8a2597215fbc9cf3
Norbert Laposa <norbert@laposa.co.uk>
2012-03-22 14:54:17 +0000

    make page cache distinguish requests with the same _GET (needed for Onxshop Wordpress plugin)

--- lib/onxshop.bootstrap.php
+++ lib/onxshop.bootstrap.php
@@ -361,8 +361,8 @@ class Onxshop_Bootstrap {
 		$backendOptions = array('cache_dir' => ONXSHOP_PROJECT_DIR . 'var/cache/');
 		
 		$cache = Zend_Cache::factory('Output', 'File', $frontendOptions, $backendOptions);
-				
-		$id = "GET_" . md5(serialize($_GET));
+		
+		$id = "GET_" . md5($request . serialize($_GET));
 		
 		if (!($data = $cache->load($id))) {
 		    // cache miss
