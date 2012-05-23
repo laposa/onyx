5d5a56894bd46491d21cfc65e1a9064d7b5c7af6
Norbert Laposa <norbert@laposa.co.uk>
2012-05-23 12:15:51 +0100

    social_nework_share improvements

---
+++ controllers/component/social_network_share.php
@@ -0,0 +1,45 @@
+<?php
+/** 
+ * Copyright (c) 2012 Laposa Ltd (http://laposa.co.uk)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ * 
+ */
+
+class Onxshop_Controller_Component_Social_Network_Share extends Onxshop_Controller {
+
+	/**
+	 * main action
+	 */
+	 
+	public function mainAction() {
+		
+		//get input
+		if (is_numeric($this->GET['node_id'])) $node_id = $this->GET['node_id'];
+		else $node_id = 5; //homepage
+		
+		//initialise
+		require_once('models/common/common_node.php');
+		require_once('models/common/common_image.php');
+		$Node = new common_node();
+		$Image = new common_image();
+		
+		//get node detail
+		$node_data = $Node->nodeDetail($node_id);
+		if ($node_data['page_title'] == '') $node_data['page_title'] = $node_data['title'];
+		
+		//get image detail
+		$image_list = $Image->listFiles($node_id);
+		if (is_array($image_list) && count($image_list) > 0) $image_detail = $image_list[0];
+		else {
+			$image_detail = array();
+			$image_detail['src'] = 'var/files/favicon.ico';
+		}
+		
+		//assign to template
+		$this->tpl->assign('IMAGE', $image_detail);
+		$this->tpl->assign('NODE', $node_data);
+		
+		return true;
+		
+	}
+}
