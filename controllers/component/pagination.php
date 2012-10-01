3398b5036404e198cf0c7b9d3d02f95e01fd2739
Hugo <dvorakp1@gmail.com>
2012-10-01 16:46:57 +0200

    update pagination to limit number of displayed items

--- controllers/component/pagination.php
+++ controllers/component/pagination.php
@@ -7,11 +7,14 @@
 
 class Onxshop_Controller_Component_Pagination extends Onxshop_Controller {
 	
+	const PAGINATION_SHOW_ITEMS = 10;
+
 	/**
 	 * main action
 	 */
 	 
 	public function mainAction() {
+
 		// input: count of all records, records per page, from which record item we display
 		// count [int], from[int], per_page[int], link[string]
 		
@@ -22,107 +25,79 @@ class Onxshop_Controller_Component_Pagination extends Onxshop_Controller {
 		$count = $this->GET['count'];
 		$from = $this->GET['limit_from'];
 		$per_page = $this->GET['limit_per_page'];
-		
-		/**
-		 * Optional input variables
-		 */ 
-		if ($this->GET['link'])  $link = $this->GET['link'];
-		else $link = $_SERVER['REDIRECT_URL'];
-		
-		//display options
-		if ($this->GET['option_show_all']) $option_show_all = $this->GET['option_show_all'];
-		else $option_show_all = 1;
-		if ($this->GET['option_per_page']) $option_per_page = $this->GET['option_per_page'];
-		else $option_per_page = 0;
-		
-		$link = translateURL(ltrim($link, '/'));
-		
-		/*
-		$count = 10;
-		$from = 2;
-		$per_page = 2;
-		$link = $_SERVER['REQUEST_URI'];
-		*/
-		
-		$pagination = array();
-		
-		
-		// generate pagination items
-		$rest = $count%$per_page;
-		if ($per_page  > 0) $pages = ($count - $rest) / $per_page;
-		if ($rest > 0) $pages++;
-		
-		//echo "pages $pages";
-		//echo "Count: $count";
-		//echo "Rest: $rest";
-		//echo "Perpage: $per_page";
-		
-		for ( $i = 1; $i <= $pages; $i++ ) {
-			if ($i == 1) $l['from'] = 0;
-			else $l['from'] = ($i - 1) * $per_page;
-			$l['per_page'] = $per_page;
-			$l['page'] = $i;
-			$classes = array();
-		
-			if ($i == 1) $classes[] = 'first';
-			if ($i == $pages) $classes[] = 'last';
-			$l['class'] = implode(' ', $classes);
-			//$l['link'] = $link;
-			$pagination[] = $l;
-		}
-		
-		//print_r($pagination);
-		
-		// pagination display
-		$i=0;
-		$display_limit_links = 25;
-		
-		foreach ($pagination as $item) {
+
+		if ($count > $per_page) {		
+
+			/**
+			 * Optional input variables
+			 */ 
+			if ($this->GET['link'])  $link = $this->GET['link'];
+			else $link = $_SERVER['REDIRECT_URL'];
 			
-			if ($item['from'] == $from && $item['per_page'] == $per_page) {
-				$previous = $item['from'] - $per_page;
-				$next = $item['from'] + $per_page;
-				$item['class'] = 'active';
-			} else {
-				$item['class'] = '';
+			// display options
+			if ($this->GET['option_show_all']) $option_show_all = $this->GET['option_show_all'];
+			else $option_show_all = 1;
+			if ($this->GET['option_per_page']) $option_per_page = $this->GET['option_per_page'];
+			else $option_per_page = 0;
+
+			// page items		
+			$pages_total = ceil($count / $per_page);
+			$current_page = floor($from / $per_page) + 1;
+
+			// setup cycle range
+			$first = max(1, $current_page - round(self::PAGINATION_SHOW_ITEMS / 2));
+			$last = $first + (self::PAGINATION_SHOW_ITEMS - 1);
+			if ($last > $pages_total) {
+				$last = $pages_total;
+				$first = max(1, $last - (self::PAGINATION_SHOW_ITEMS - 1));
 			}
-		
-			$item['link'] = $link;
-			if ($item['from'] == 0) $limit = '';
-			else $limit = "?limit_from={$item['from']}&amp;limit_per_page={$item['per_page']}";
-			$this->tpl->assign('LIMIT', $limit);
-		
-			$this->tpl->assign('ITEM', $item);
-	
-			//display pagination item only if there are multiple pages and still less than display_limit_links
-			if ($item['per_page'] > 0 && $i < $display_limit_links) $this->tpl->parse('content.pagination.item');
-		
-			$i++;
+
+			for ($i = $first; $i <= $last; $i++) {
+
+				$limit_from = ($i - 1) * $per_page;
+
+				if ($limit_from == 0) $limit = '';
+				else $limit = "?limit_from=$limit_from&amp;limit_per_page=$per_page";
+
+				$item = array(
+					'class' => $i == $current_page ? 'active' : '',
+					'page' => $i,
+					'link' => $link
+				);
+
+				$this->tpl->assign('LIMIT', $limit);
+				$this->tpl->assign('ITEM', $item);
+				$this->tpl->parse('content.pagination.item');
+			}
+
+			// previous
+			if ($from > 0) {
+				$previous = max($from - $per_page, 0);
+				if ($previous == 0) $limit = '';
+				else $limit = "?limit_from={$previous}&amp;limit_per_page=$per_page";
+				$this->tpl->assign('LIMIT', $limit);
+				$this->tpl->parse('content.pagination.previous');
+			}
+			
+			// next
+			$next = $from + $per_page;
+			if ($next < $count)  {
+				$limit = "?limit_from={$next}&amp;limit_per_page=$per_page";
+				$this->tpl->assign('LIMIT', $limit);
+				$this->tpl->parse('content.pagination.next');
+			}
+			
+			// display options?
+			if ($option_show_all) $this->tpl->parse('content.pagination.show_all');
+			if ($option_per_page) {
+				$this->tpl->assign("PAGINATION_selected_$per_page", 'selected="selected"');
+				$this->tpl->parse('content.pagination.per_page');
+			}
+			
+			// parse template
+			 $this->tpl->parse('content.pagination');
+
 		}
-		
-		//previous
-		if ($from > 0) {
-			if ($previous == 0) $limit = '';
-			else $limit = "?limit_from={$previous}&amp;limit_per_page={$item['per_page']}";
-			$this->tpl->assign('LIMIT', $limit);
-			$this->tpl->parse('content.pagination.previous');
-		}
-		
-		//next
-		if ($next < $count)  {
-			$limit = "?limit_from={$next}&amp;limit_per_page={$item['per_page']}";
-			$this->tpl->assign('LIMIT', $limit);
-			$this->tpl->parse('content.pagination.next');
-		}
-		
-		//display options?
-		if ($option_show_all) $this->tpl->parse('content.pagination.show_all');
-		if ($option_per_page) {
-			$this->tpl->assign("PAGINATION_selected_$per_page", 'selected="selected"');
-			$this->tpl->parse('content.pagination.per_page');
-		}
-		
-		if ($count > $per_page) $this->tpl->parse('content.pagination');
 
 		return true;
 	}
