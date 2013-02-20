7f074d3e043d4f72434c4ee45d6928a08546a9be
Hugo <dvorakp1@gmail.com>
2013-02-20 18:46:52 +0100

    node duplication to include related images and nested nodes

--- controllers/bo/component/node_duplicate.php
+++ controllers/bo/component/node_duplicate.php
@@ -6,6 +6,9 @@
  * 
  */
 
+require_once('models/common/common_node.php');
+require_once('models/common/common_image.php');
+
 class Onxshop_Controller_Bo_Component_Node_Duplicate extends Onxshop_Controller {
 
 	/**
@@ -13,35 +16,68 @@ class Onxshop_Controller_Bo_Component_Node_Duplicate extends Onxshop_Controller 
 	 */
 	 
 	public function mainAction() {
-		/*
-		 * 1. get old detail
-		 * 2. insert new
-		 * 3. check for related images on old 
-		 * 4. duplicate images
-		 */
-		require_once('models/common/common_node.php');
 		
 		if (is_numeric($this->GET['id'])) $original_node_id = $this->GET['id'];
 		else return false;
 		
-		$Node = new common_node();
+		$this->Node = new common_node();
+		$this->Image = new common_image();
 		
-		$original_node_data = $Node->detail($original_node_id);
+		$new_node_id = $this->duplicateNode($original_node_id);
+
+		if ($new_node_id) {
+			msg("Content successfully duplicated.");
+
+			$_nSite = new nSite("node~id=$new_node_id~");
+			$this->tpl->assign('NODE_DETAIL', $_nSite->getContent());
+		}
+
+		return $new_node_id > 0;
+	}
+
+
+	/**
+	 * recursivelly duplicate node and its contens
+	 */
+
+	protected function duplicateNode($original_node_id, $new_parent_id = false)
+	{
+		// read original node
+		$original_node_data = $this->Node->detail($original_node_id);
 		$new_node_data = $original_node_data;
 		$new_node_data['title'] = "{$new_node_data['title']} (copy)";
+		if ($new_parent_id > 0) $new_node_data['parent'] = $new_parent_id;
 		unset($new_node_data['id']);
-		
-		$new_node_id = $Node->insert($new_node_data);
+
+		// insert as new
+		$new_node_id = $this->Node->insert($new_node_data);
 		if (!is_numeric($new_node_id)) {
 			msg('node_duplicate: Cannot create node', 'error');
 			return false;
 		}
-		
-		msg("Duplicated. Please note that associated images were not copied over.");
-		
-		$_nSite = new nSite("node~id=$new_node_id~");
-		$this->tpl->assign('NODE_DETAIL', $_nSite->getContent());
-		
-		return true;
+
+		// read related images
+		$original_images = $this->Image->listing("node_id = $original_node_id");
+
+		// duplicate images
+		if (is_array($original_images)) {
+			foreach ($original_images as $image) {
+				$new_image = $image;
+				$new_image['node_id'] = $new_node_id;
+				unset($new_image['id']);
+				$image_id = $this->Image->insert($new_image);
+			}
+		}
+
+		// read and duplicate nested nodes
+		$nested_nodes = $this->Node->listing("parent = $original_node_id");
+		if (is_array($nested_nodes)) {
+			foreach ($nested_nodes as $nested_node) {
+				$this->duplicateNode($nested_node['id'], $new_node_id);
+			}
+		}
+
+		return $new_node_id;
 	}
+
 }
