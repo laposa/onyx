9c4c731064ab6edbaf278ef4cf50ccac67628126
Norbert Laposa <norbert@laposa.co.uk>
2013-03-26 15:40:26 +0000

    add classes for new tables

---
+++ models/client/client_customer_taxonomy.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright (c) 2013 Laposa Ltd (http://laposa.co.uk)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ *
+ */
+ 
+require_once('models/common/common_node_taxonomy.php');
+
+class client_customer_taxonomy extends common_node_taxonomy {
+}
