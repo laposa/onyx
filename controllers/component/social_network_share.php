d9fa11e43e70372b1c0cca71a470f5b471bb40ad
Hugo Dvorak <dvorakp1@gmail.com>
2013-04-12 16:32:04 +0200

    refactor social network share component to allow better class inheritance

--- controllers/component/social_network_share.php
+++ controllers/component/social_network_share.php
@@ -5,45 +5,69 @@
  * 
  */
 
+require_once('models/common/common_node.php');
+require_once('models/common/common_image.php');
+
 class Onxshop_Controller_Component_Social_Network_Share extends Onxshop_Controller {
+
+	// models
+	public $Node;
+	public $Image;
+
+	// key
+	public $node_id;
+
+	// data structures
+	public $node;
+	public $image;
 
 	/**
 	 * main action
 	 */
-	 
+
 	public function mainAction() {
-		
-		//get input
-		if (is_numeric($this->GET['node_id'])) $node_id = $this->GET['node_id'];
-		else $node_id = 5; //homepage
-		
-		//initialise
-		require_once('models/common/common_node.php');
-		require_once('models/common/common_image.php');
-		$Node = new common_node();
-		$Image = new common_image();
-		
-		//get node detail
-		$node_data = $Node->nodeDetail($node_id);
-		if ($node_data['page_title'] == '') $node_data['page_title'] = $node_data['title'];
-		
-		//set URI
-		$share_uri = "http://".$_SERVER['HTTP_HOST']."/page/$node_id";
+
+		$this->initModels();
+		$this->node_id = $this->getNodeId();
+		$this->node = $this->getNode($this->node_id);
+		$this->image = $this->getImage($this->node_id);
+
+		$share_uri = "http://".$_SERVER['HTTP_HOST']."/page/{$this->node_id}";
+
 		$this->tpl->assign('SHARE_URI', $share_uri);
-		
-		//get image detail
-		$image_list = $Image->listFiles($node_id);
-		if (is_array($image_list) && count($image_list) > 0) $image_detail = $image_list[0];
-		else {
-			$image_detail = array();
-			$image_detail['src'] = 'var/files/favicon.ico';
-		}
-		
-		//assign to template
-		$this->tpl->assign('IMAGE', $image_detail);
-		$this->tpl->assign('NODE', $node_data);
-		
+		$this->tpl->assign('IMAGE', $this->image);
+		$this->tpl->assign('NODE', $this->node);
+
 		return true;
 		
+	}
+
+	protected function initModels() {
+
+		$this->Node = new common_node();
+		$this->Image = new common_image();
+
+	}
+
+	protected function getNodeId() {
+
+		if (is_numeric($this->GET['node_id'])) return $this->GET['node_id'];
+		return 5; //homepage
+
+	}
+
+	protected function getNode($node_id) {
+
+		$node_data = $this->Node->nodeDetail($node_id);
+		if ($node_data['page_title'] == '') $node_data['page_title'] = $node_data['title'];
+		return $node_data;
+	}
+
+	protected function getImage($node_id) {
+
+		$image_list = $this->Image->listFiles($node_id);
+		if (is_array($image_list) && count($image_list) > 0) return $image_list[0];
+		return array('src' => 'var/files/favicon.ico');
+
 	}
 }
