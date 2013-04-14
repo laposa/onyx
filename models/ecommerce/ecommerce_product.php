f5548991db34e86b52b9afb9a1818d9d57dae1a9
Norbert Laposa <norbert@laposa.co.uk>
2013-04-14 14:15:57 +0100

    transition from filter[disabled] to filter[publish]

--- models/ecommerce/ecommerce_product.php
+++ models/ecommerce/ecommerce_product.php
@@ -380,9 +380,9 @@ CREATE TABLE ecommerce_product (
 	    	//stock value
 	    	if (is_numeric($filter['stock']) && $filter['stock'] >= 0) $add_to_where .= " AND variety.stock < {$filter['stock']}";
 	    	
-	    	//disable/enabled
-	    	if ($filter['disabled'] == 'disabled') $add_to_where .= " AND product.publish = 0";
-	    	else if ($filter['disabled'] == 'enabled') $add_to_where .= " AND product.publish = 1 AND variety.publish = 1";
+	    	//publish filter and deprecated disable/enabled option
+	    	if ($filter['publish'] === 0 || $filter['disabled'] == 'disabled') $add_to_where .= " AND product.publish = 0";
+	    	else if ($filter['publish'] === 1  || $filter['disabled'] == 'enabled') $add_to_where .= " AND product.publish = 1 AND variety.publish = 1";
 	    	
 	    	//taxonomy
 	    	if ($filter['taxonomy_json']) {
