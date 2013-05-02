7011e1567a72e0df60cdd09d58e635edf3f5dfc0
Norbert Laposa <norbert@laposa.co.uk>
2013-05-02 16:20:46 +0100

    reorganise CSS and make using traditional gutters by default (on each content element)

--- publish.sh
+++ publish.sh
@@ -40,6 +40,7 @@ echo "------------------------------------------------";
 echo "COMPILING ${SCREEN_CSS}";
 echo "------------------------------------------------";
 cat ${FULL_PATH}/share/css/default/src/global.css > ${SCREEN_CSS};
+cat ${FULL_PATH}/share/css/default/src/layout.css >> ${SCREEN_CSS};
 cat ${FULL_PATH}/share/css/default/src/onxshop.css >> ${SCREEN_CSS};
 cat ${FULL_PATH}/share/css/default/src/onxshop_ecommerce.css >> ${SCREEN_CSS};
 
