1c6f57d12cda6f4d3fde91283f2daf52e8ba35ec
Norbert Laposa <norbert@laposa.co.uk>
2013-05-03 16:41:30 +0100

    new component menu_select

---
+++ controllers/component/menu_select.php
@@ -0,0 +1,21 @@
+<?php
+/** 
+ * Copyright (c) 2013 Laposa Ltd (http://laposa.co.uk)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ *
+ */
+
+require_once('controllers/component/menu.php');
+
+class Onxshop_Controller_Component_Menu_Select extends Onxshop_Controller_Component_Menu {
+
+	public function parseItem($item)
+	{
+		if (in_array($item['id'], $this->full_path)) {
+			$item['selected'] = 'selected="selected"';
+		}
+
+		return parent::parseItem($item);
+	}
+
+}
