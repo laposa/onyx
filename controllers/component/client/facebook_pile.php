e579327aea2e57f8ed87cdc80be758cf26a6f0a9
Hugo Dvorak <hugo@dvorak.io>
2013-09-06 15:53:12 +0100

    upgrade facebook pile compontent to cooperate with Open Graph

--- controllers/component/client/facebook_pile.php
+++ controllers/component/client/facebook_pile.php
@@ -6,143 +6,216 @@
  */
 
 require_once 'controllers/component/client/facebook.php';
+require_once 'models/client/client_action.php';
+require_once 'models/client/client_customer.php';
 
+/**
+ * Display recent frinds actions in similar manner as Facebook pile
+ *
+ * Parameters:
+ * 	num_displayed_items - number of displayed items (3 by default)
+ * 	action_filter - filter displayed items to specific actions (comma separated list, include 
+ * 	                action-object pairs, e.g. comment_on-recipe, enter-competition, etc.)
+ * 	fetch_stream - fetch actions directly from facebook stream instead of local client_action database
+ * 	
+ */
 class Onxshop_Controller_Component_Client_Facebook_Pile extends Onxshop_Controller_Component_Client_Facebook {
+
+	protected $num_displayed_items = 3;
+	protected $action_filter = null;
+	protected $fetch_stream = false;
 
 	/**
 	 * main action
 	 */
 	 
 	public function mainAction() {
+
+		$this->processInput($this->GET);
+
+		$this->Node = new common_node(); // init model
+		$this->commonAction(); // init Facebook SDK
 		
-		/**
-		 * input
-		 */
-		
-		if ($this->GET['fb_username']) $fb_username = $this->GET['fb_username'];
-		else $fb_username = 'me';
-		
-		if (is_numeric($this->GET['show_number_of_items'])) $this->show_number_of_items = $this->GET['show_number_of_items'];
-		else $this->show_number_of_items = 3;
-		
-		/**
-		 * initialize
-		 */
-		 
-		$Node = new common_node();
-		
-		
-		/**
-		 * call shared actions
-		 */
-		 
-		$this->commonAction();
-		
-		$user = $this->Facebook->getUser();
-		
-		if ($user) {
-			
-			$activity_list = $this->getFriendsActivity();
-			
-			$activity_list_count = count($activity_list);
-			
-			if (is_array($activity_list)) {
-			
-				$i = 0;
-				
-				foreach ($activity_list as $item) {
-				
-					if ($i < $this->show_number_of_items) {
-					
-						try {
-						
-							$post_detail = $this->Facebook->api('/' . $item['post_id']);
-						
-						} catch (FacebookApiException $e) {
-						
-							msg($e->getMessage(), 'error', 1);
-							
-						}
-						
-						if (preg_match('/[0-9]*$/', $post_detail['link'], $matches)) {
-							
-							$node_id = $matches[0];
-							$node_detail = $Node->getDetail($node_id);
-							
-							//cut off long titles
-							if (strlen($node_detail['title']) > 32) $node_detail['title'] = substr($node_detail['title'], 0, 32) . '…' ;
-							
-							$this->tpl->assign('NODE', $node_detail);
-							$this->tpl->assign('FACEBOOK_POST', $post_detail);
-							$this->tpl->parse('content.item_activity');
-							$i++;
-						}
-						
-					}
-				
-				}
+		if ($user = $this->Facebook->getUser()) {
+
+			$friend_user_list = $this->getFriendsAppUsers();
+			if (count($friend_user_list) == 0) return true; // not friends use this app
+
+			if ($this->fetch_stream) {
+
+				// fetch stream from Facebook
+				$activity_list = $this->readFriendsStream($friend_user_list);
+				$parsedItems = $this->parseFacebookActivities($activity_list);
+
+			} else {
+
+				// fetch actions from local database
+				$friends_customer_ids = $this->facebookToCustomerIds($friend_user_list);
+				$actions = $this->getActionsForCustomers($friends_customer_ids, $this->num_displayed_items * 2, $this->action_filter);
+				$parsedItems = $this->parseLocalActions($actions, $this->num_displayed_items);
+
 			}
-			
-			/**
-			 * use user list
-			 */
-			
-			if ($activity_list_count < $this->show_number_of_items) {
-				
-				$friend_user_list = $this->getFriendsAppUsers();
-				
-				$i = 0;
-				
-				if (is_array($friend_user_list)) {
-				
-					foreach ($friend_user_list as $item) {
-						
-						if ($i < ($this->show_number_of_items - $activity_list_count)) {
-							
-							try {
-	
-								$user_detail = $this->Facebook->api("/{$item}");
-	
-							} catch (FacebookApiException $e) {
-			
-								msg($e->getMessage(), 'error', 1);
-								
-							}
-							
-							$this->tpl->assign('FACEBOOK_USER', $user_detail);
-							$this->tpl->parse('content.item_friend_user');
-							
-							$i++;
-							
-						}
-					}
-				}
-			}
-			
-			/**
-			 * show title only if at least one item is listed
-			 */
-			 
-			$friend_user_list_count = count($friend_user_list);
-			
-			$total_list_count = $activity_list_count + $friend_user_list_count;
-			
-			if ($total_list_count > 0) $this->tpl->parse('content.title');
-			
+
+			if ($parsedItems < $this->num_displayed_items) $parsedItems += $this->parseFriendList($friend_user_list, $this->num_displayed_items - $parsedItems);
 		}
-		
+
+		if ($parsedItems > 0) $this->tpl->parse('content.title');
+
 		return true;
-		
+
 	}
-	
+
 	/**
-	 * getFriends
+	 * Parse facebook activities
+	 */
+	protected function parseFacebookActivities($activity_list)
+	{
+		$parsedItems = 0;
+
+		if (is_array($activity_list)) {
+		
+			foreach ($activity_list as $item) {
+			
+				$post_detail = $this->makeApiCallCached("/{$item['post_id']}");
+
+				if (preg_match('/[0-9]+$/', $post_detail['link'], $matches)) {
+					
+					$node_id = $matches[0];
+					$node_detail = $this->Node->getDetail($node_id);
+					
+					//cut off long titles
+					if (strlen($node_detail['title']) > 32) $node_detail['title'] = substr($node_detail['title'], 0, 32) . '…' ;
+					
+					$this->tpl->assign('NODE', $node_detail);
+					$this->tpl->assign('FACEBOOK_POST', $post_detail);
+					$this->tpl->assign('ACTION', "recommended");
+					$this->tpl->parse('content.item_activity');
+					$parsedItems++;
+				}
+
+				if ($parsedItems == $num_displayed_items) return $parsedItems;					
+				
+			}
+		}
+
+		return $parsedItems;
+
+	}
+
+	/**
+	 * Parse friendlist
+	 */
+	protected function parseFriendList($friend_user_list, $num_displayed_items)
+	{
+		$parsedItems = 0;
+
+		if (is_array($friend_user_list)) {
+		
+			foreach ($friend_user_list as $item) {
+				
+				$user_detail = $this->makeApiCallCached("/{$item}");
+				
+				if ($user_detail) {
+
+					$this->tpl->assign('FACEBOOK_USER', $user_detail);
+					$this->tpl->parse('content.item_friend_user');
+					
+					$parsedItems++;
+				}
+
+				if ($parsedItems == $num_displayed_items) return $parsedItems;					
+			}
+		}
+
+		return $parsedItems;
+	}
+
+	/**
+	 * Parse local actions
+	 */
+	protected function parseLocalActions($actions, $num_displayed_items)
+	{
+		$parsedItems = 0;
+
+		foreach ($actions as $action) {
+
+			$post_detail = $this->makeApiCallCached("/{$action['action_id']}");
+
+			if ($post_detail) {
+
+				$node_detail = $this->Node->getDetail($action['node_id']);
+				
+				//cut off long titles
+				if (strlen($node_detail['title']) > 32) $node_detail['title'] = substr($node_detail['title'], 0, 32) . '…' ;
+				
+				$this->tpl->assign('NODE', $node_detail);
+				$this->tpl->assign('FACEBOOK_POST', $post_detail);
+				$this->tpl->assign('ACTION', $this->getPastTense($action['action_name']));
+				$this->tpl->parse('content.item_activity');
+				$parsedItems++;
+
+			}
+
+			if ($parsedItems == $num_displayed_items) return $parsedItems;
+		}
+
+		return $parsedItems;
+
+	}
+
+	/**
+	 * Process input parameters
+	 * @param  array $input Array (GET)
+	 */
+	protected function processInput($input)
+	{
+		// numberg of displayed items (3 by default)
+		if (is_numeric($input['num_displayed_items'])) $this->num_displayed_items = $input['num_displayed_items'];
+
+		// allow to display only specific actions (comma separated list)
+		if (strlen($input['action_filter']) > 0) $this->action_filter = explode(",", $input['action_filter']);
+
+		// allow to display stream activities fetched directly from Facebook
+		if ($input['fetch_stream'] == 1) $this->fetch_stream = true;
+
+	}	
+
+	/**
+	 * Get action past tense (enter => entered, etc.)
+	 * @param  String $action Action verb present tense
+	 * @return String
+	 */
+	protected function getPastTense($action)
+	{
+		switch ($action) {
+			case 'comment_on': return 'commented on';
+			case 'enter': return 'entered';
+			case 'vote_in': return 'voted in';
+			case 'like': return 'likes';
+		}
+		return 'recommended';
+	}
+
+	/**
+	 * Read friends stream from Facebook
 	 */
 	 
-	public function getFriends($facebook_user_id) {
+	protected function readFriendsStream($friend_user_list) {
 	
-		$fql = "SELECT uid, first_name, last_name FROM user WHERE uid in (SELECT uid2 FROM friend where uid1 = $facebook_user_id)";
-		
+		$friends = $this->prepareListForSql($friend_user_list, true);
+
+		if (!$friends) return false;
+
+		$fql = "SELECT app_id, type, created_time, post_id, actor_id, message, action_links, 
+			description, permalink
+			FROM stream
+			WHERE filter_key IN (
+				SELECT filter_key 
+				FROM stream_filter 
+				WHERE uid = me()
+			) AND actor_id IN ($friends)
+			AND app_id = " . ONXSHOP_FACEBOOK_APP_ID . " LIMIT 3";
+	
 		$response = $this->makeApiCallCached(array(
 				'method' => 'fql.query',
 				'query' =>$fql
@@ -152,30 +225,93 @@ class Onxshop_Controller_Component_Client_Facebook_Pile extends Onxshop_Controll
 	}
 	
 	/**
-	 * getFriendsActivity
+	 * Get list of friends who use the app
 	 */
 	 
-	public function getFriendsActivity() {
-	
-		$fql = "SELECT app_id, type, created_time, post_id, actor_id, message, action_links, description, permalink FROM stream WHERE filter_key IN (SELECT filter_key FROM stream_filter WHERE uid = me()) AND actor_id IN (SELECT uid2 FROM friend WHERE uid1 = me()) AND app_id = " . ONXSHOP_FACEBOOK_APP_ID . " LIMIT 3";
-		
-		$response = $this->makeApiCallCached(array(
-				'method' => 'fql.query',
-				'query' =>$fql
-			));
-		
-		return $response;
-	}
-	
-	/**
-	 * getFriendsAppUsers
-	 */
-	 
-	public function getFriendsAppUsers() {
+	protected function getFriendsAppUsers() {
 		
 		$response = $this->makeApiCallCached(array('method' => 'friends.getAppUsers'));
 		
 		return $response;
 	}
- 	
+
+	/**
+	 * Convert facebook user ids to local customer ids
+	 * 
+	 * @return array
+	 */
+	protected function facebookToCustomerIds($facebook_ids)
+	{
+		$result = array();
+
+		if (is_array($facebook_ids) && count($facebook_ids) > 0 && $ids = $this->prepareListForSql($facebook_ids, true)) {
+
+			$Customer = new client_customer();
+			$list = $Customer->listing("facebook_id IN ($ids)");
+			if (is_array($list) && count($list) > 0) foreach ($list as $item) $result[] = $item['id'];
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Convert list of identifiers to comma separated values
+	 * @param  Array   $list   Array of values
+	 * @param  boolean $escape Escape values for SQL?
+	 * @return String
+	 */
+	protected function prepareListForSql($list, $escape = false)
+	{
+		$result = false;
+
+		if (is_array($list) && count($list) > 0) {
+
+			$items = array();
+			foreach ($list as $item) {
+				if ($escape) $items[] = pg_escape_string(trim($item));
+				else $items[] = (int) trim($item);
+			}
+
+			$result = implode(",", $items);
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Get list of actions performed by given customers from local database
+	 * 
+	 * @param  array $customer_ids Customers id to query
+	 * @return Array of client_action
+	 */
+	protected function getActionsForCustomers($customer_ids, $num_displayed_items = 3, $filter = false)
+	{
+		$result = array();
+
+		if (is_array($customer_ids) && count($customer_ids) > 0 && $ids = $this->prepareListForSql($customer_ids))  {
+		
+			$Action = new client_action();
+
+			$filter_sql = '';
+			if (is_array($filter) && count($filter) > 0) {
+				$filter_sql = ' AND (';
+				foreach ($filter as $action) {
+					$action = explode("-", $action);
+					if (count($action) == 2) {
+						$filter_sql .= "(action_name = '" . pg_escape_string($action[0]) . "'";
+						$filter_sql .= " AND object_name = '" . pg_escape_string($action[1]) . "') OR ";
+					}
+				}
+				$filter_sql .= '(1 = 0))';
+			}
+
+			$list = $Action->listing("network = 'facebook' AND customer_id IN ($ids) $filter_sql", 
+				"id DESC", "0,{$num_displayed_items}");
+			if (is_array($list) && count($list) > 0) foreach ($list as $item) $result[] = $item;
+
+		}
+
+		return $result;
+	}
+
 }
--- controllers/component/client/facebook_story_create.php
+++ controllers/component/client/facebook_story_create.php
@@ -17,13 +17,25 @@ class Onxshop_Controller_Component_Client_Facebook_Story_Create extends Onxshop_
 
 		$this->node_id = $this->GET['node_id'];
 		$this->action = $this->GET['action'];
-		if (empty($this->GET['object'])) $this->object = $this->getObjectName($this->node_id);
-		else $this->object = $this->GET['object'];
+
+		$object_name = $this->getObjectName($this->node_id);
+
+		if (empty($this->GET['object'])) {
+			$this->object = $object_name;
+		}
+		else {
+			$this->object = $this->GET['object'];
+			if ($this->object != $object_name) msg("facebook_story_create: object name '{$this->object}' " .
+				" and shared node (id: {$this->node_id}) type mismatch", "error", 1);
+		}
 
 		if (!$this->validateInput()) return true;
 
 		$this->commonAction();
-		$response = $this->makeFacebookPost();
+
+		if ($this->action == 'like') $response = $this->postFacebookLike();
+		else $response = $this->postFacebookAction();
+			
 		$this->processResponse($response);
 
 		return true;
@@ -31,13 +43,41 @@ class Onxshop_Controller_Component_Client_Facebook_Story_Create extends Onxshop_
 
 
 
-	public function makeFacebookPost()
+	public function postFacebookLike()
+	{
+		try {
+
+			$response = $this->Facebook->api(
+				'me/og.likes', 'POST',
+				array('object' => $this->getShareUri())
+			);
+
+		} catch (FacebookApiException $e) {
+
+			// object already liked
+			if (strpos($e->getMessage(), "#3501")) {
+				preg_match('/Action ID: (\d+)/', $e->getMessage(), $matches);
+				// open activity so user can unlike it
+				header("Location: https://www.facebook.com/me/activity/{$matches[1]}/");
+				exit();
+			}
+
+			msg($e->getMessage(), 'error', 1);
+
+			return null;
+		}
+
+		return $response;
+	}
+
+
+	public function postFacebookAction()
 	{
 		$response = $this->makeApiCall(
 			'me/' . ONXSHOP_FACEBOOK_APP_NAMESPACE . ":" . $this->action, 'POST',
 			array($this->object => $this->getShareUri())
 		);
-		
+
 		return $response;
 	}
 
@@ -59,7 +99,11 @@ class Onxshop_Controller_Component_Client_Facebook_Story_Create extends Onxshop_
 			case 'store':
 			case 'recipe':
 			case 'article':
+			case 'poll':
+			case 'competition':
 				return $node['node_controller'];
+			case 'default':
+				return 'article';
 		}
 
 		return false;
@@ -109,8 +153,20 @@ class Onxshop_Controller_Component_Client_Facebook_Story_Create extends Onxshop_
 				. ":object_name=" . $this->object
 				. "~");
 
+			if ($this->GET['redirect_to_post']) {
+				header("Location: https://www.facebook.com/me/activity/{$response['id']}/");
+				exit();
+			}
+
 		} else {
+
 			msg("Unable to create story on Facebook " . print_r($response, TRUE), 'error', 1);
+
+			if ($this->GET['redirect_to_post']) {
+				echo("<script>window.close()</script>");
+				exit();
+			}
+
 		}
 	} 	
 
