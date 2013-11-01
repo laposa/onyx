6237bf92b649c1140dd169003bcdcc28c04696b7
Hugo Dvorak <hugo@laposa.co.uk>
2013-11-01 16:16:56 +0000

    implement combobox for product highlights component

--- share/js/backoffice.js
+++ share/js/backoffice.js
@@ -61,3 +61,97 @@ $(function() {
 	$('select option.disabled, select option.publish_0').append(' (not public)');
 	
 });
+
+/**
+ * jQuery UI comboxo widget
+ */
+$.widget("custom.combobox", {
+	_create: function() {
+		this.wrapper = $("<span>").addClass("custom-combobox").insertAfter(this.element);
+		this.element.hide();
+		this._createAutocomplete();
+		this._createShowAllButton();
+	},
+
+	_createAutocomplete: function() {
+		var selected = this.element.children( ":selected" ),
+		value = selected.val() ? selected.text() : "";
+
+		this.input = $("<input>").appendTo(this.wrapper).val( value).attr("title", "").addClass("custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left").autocomplete({
+			delay: 0,
+			minLength: 0,
+			source: $.proxy( this, "_source"),
+		}).tooltip({tooltipClass: "ui-state-highlight"});
+
+		this.input.data("ui-autocomplete")._renderItem = function(ul, item) {
+			var c = '';
+			if (item.option.disabled) c += 'disabled ';
+			if ($(item.option).attr("data-class") == 'notpublic') c += 'notpublic ';
+      		return $("<li>").append("<a class=\"" + c + "\">" + item.label + "</a>").appendTo(ul);
+    	};
+
+		this._on(this.input, {
+			autocompleteselect: function(event, ui) {
+				ui.item.option.selected = true;
+				this._trigger("select", event, {item: ui.item.option});
+			},
+
+			autocompletechange: "_removeIfInvalid"
+		});
+	},
+
+	_createShowAllButton: function() {
+		var input = this.input,
+		wasOpen = false;
+
+		$("<a>").attr("tabIndex", -1).appendTo(this.wrapper).button({
+			icons: { primary: "ui-icon-triangle-1-s" },
+			text: false
+		}).removeClass("ui-corner-all").addClass("custom-combobox-toggle ui-corner-right").mousedown(function() {
+			wasOpen = input.autocomplete("widget").is( ":visible" );
+		}).click(function() {
+			input.focus();
+			if (wasOpen) return;
+			input.autocomplete("search", "");
+		});
+	},
+
+	_source: function(request, response) {
+		var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
+		response(this.element.children("option").map(function() {
+			var text = $(this).text();
+			if (this.value && (!request.term || matcher.test(text))) return {
+				label: text,
+				value: text,
+				option: this
+			};
+		}) );
+	},
+
+	_removeIfInvalid: function( event, ui ) {
+
+		if ( ui.item ) return;
+
+		var value = this.input.val(),
+		valueLowerCase = value.toLowerCase(),
+		valid = false;
+		this.element.children("option").each(function() {
+			if ( $( this ).text().toLowerCase() === valueLowerCase ) {
+				this.selected = valid = true;
+				return false;
+			}
+		});
+
+		if ( valid ) return;
+
+		this.input.val("").attr("title", value + " didn't match any item").tooltip("open");
+		this.element.val("");
+		this._delay(function() { this.input.tooltip("close").attr("title", ""); }, 2500 );
+		this.input.data("ui-autocomplete").term = "";
+	},
+
+	_destroy: function() {
+		this.wrapper.remove();
+		this.element.show();
+	}
+});
