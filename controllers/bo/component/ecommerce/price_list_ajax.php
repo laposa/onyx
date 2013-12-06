2216ee0ac91f6dd8b877c06058f6a19ab181aa57
Hugo Dvorak <hugo@dvorak.io>
2013-12-06 13:53:13 +0000

    new price_list component for ajax use

---
+++ controllers/bo/component/ecommerce/price_list_ajax.php
@@ -0,0 +1,24 @@
+<?php
+/** 
+ * Copyright (c) 2006-2013 Laposa Ltd (http://laposa.co.uk)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ * 
+ */
+
+class Onxshop_Controller_Bo_Component_Ecommerce_Price_List_Ajax extends Onxshop_Controller {
+	
+	/**
+	 * main action
+	 */
+	 
+	public function mainAction() {
+		
+		require_once('models/ecommerce/ecommerce_price.php');
+		$Price = new ecommerce_price();
+		
+		$price_list = $Price->getPriceList($this->GET['product_variety_id']);
+		$this->tpl->assign('CONTENT', json_encode($price_list));
+
+		return true;
+	}
+}
