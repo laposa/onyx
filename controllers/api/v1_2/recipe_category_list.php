48d1767c22fbbfd04e4b5914ef5b0c3d8fa26967
Norbert Laposa <norbert@laposa.co.uk>
2014-02-06 16:43:40 +0000

    bug fixes in API

--- controllers/api/v1_2/recipe_category_list.php
+++ controllers/api/v1_2/recipe_category_list.php
@@ -7,6 +7,6 @@
 
 require_once('controllers/api/v1_1/recipe_category_list.php');
 
-class Onxshop_Controller_Api_v1_2_Recipe_Category_List extends Onxshop_Controller_Api_v1_2_Recipe_Category_List {
+class Onxshop_Controller_Api_v1_2_Recipe_Category_List extends Onxshop_Controller_Api_v1_1_Recipe_Category_List {
 	
 }
