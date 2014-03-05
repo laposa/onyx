5e1a2fa1057d559aff0d2765271b4510a985d7f3
Hugo Dvorak <hugo@dvorak.io>
2014-03-05 14:51:20 +0100

    allow sent notifications to be exported as CSV from back office

--- controllers/bo/component/ecommerce/stock.php
+++ controllers/bo/component/ecommerce/stock.php
@@ -85,6 +85,35 @@ class Onxshop_Controller_Bo_Component_Ecommerce_Stock extends Onxshop_Controller
 			msg("No products found", 'error');
 		}
 
+		$this->parseNotificationsExport();
 		return true;
 	}
+
+	public function parseNotificationsExport()
+	{
+		$month = date("m");
+		$year = date("Y");
+
+		for ($i = 0; $i < 24; $i++) {
+
+			$time = strtotime("$year-$month-01");
+
+			$date = array(
+				"from" => date("Y-m-01", $time),
+				"to" => date("Y-m-t", $time),
+				"label" => date("m/Y", $time),
+			);
+
+			$this->tpl->assign("DATE", $date);
+			$this->tpl->parse("content.stock_period");
+
+			$month--;
+			if ($month == 0) {
+				$year--;
+				$month = 12;
+			}
+
+		}
+	}
+
 }		
