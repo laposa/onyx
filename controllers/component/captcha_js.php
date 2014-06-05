76f69f5889e4bf2d5cf069eb1252e96ed11a623b
Hugo Dvorak <hugo@dvorak.io>
2014-06-05 23:35:45 +0100

    refactor captcha_js to allow broader usage (e.g. in comments)

--- controllers/component/captcha_js.php
+++ controllers/component/captcha_js.php
@@ -12,34 +12,40 @@ class Onxshop_Controller_Component_Captcha_Js extends Onxshop_Controller {
 	 
 	public function mainAction() {
 
-		/** 
-		 * get input variables
-		 */
-
 		$node_id = (int) $this->GET['node_id'];
 
-		if ($node_id > 0) {
+		if ($node_id > 0 && $this->nodeUsesCaptcha($node_id)) {
 
-			// confirm the noded uses javascript captcha
-			require_once('models/common/common_node.php');
-			$Node = new common_node();
-			$node_data = $Node->nodeDetail($node_id);
+			$this->showCode($node_id);
 
-			if ($node_data['component']['spam_protection'] =='captcha_text_js' || 
-				$node_data['node_controller'] == 'news') {
-
-				// captcha
-				$word = $this->generateRandomWord();
-
-				// save code to session
-				if (!is_array($_SESSION['captcha'])) $_SESSION['captcha'] = array();
-				$_SESSION['captcha'][$node_id] = $word;
-
-				$this->tpl->assign("CODE", $word);
-			}
 		}
 
 		return true;
+	}
+
+	protected function nodeUsesCaptcha($node_id)
+	{
+		if ($this->GET['nocheck']) return true;
+
+		// confirm the noded uses javascript captcha
+		require_once('models/common/common_node.php');
+		$Node = new common_node();
+		$node_data = $Node->nodeDetail($node_id);
+
+		return ($node_data['component']['spam_protection'] =='captcha_text_js' || 
+				$node_data['node_controller'] == 'news');
+	}
+
+	protected function showCode($node_id)
+	{
+		// captcha
+		$word = $this->generateRandomWord();
+
+		// save code to session
+		if (!is_array($_SESSION['captcha'])) $_SESSION['captcha'] = array();
+		$_SESSION['captcha'][$node_id] = $word;
+
+		$this->tpl->assign("CODE", $word);
 	}
 
 	protected function generateRandomWord($length = 5)
