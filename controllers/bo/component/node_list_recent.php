d71ae67f9d3d3cabf1b0c9e7e716a527826348c7
Norbert Laposa <norbert@laposa.co.uk>
2014-07-10 12:30:40 +0100

    add recent edits to bo / summary page

---
+++ controllers/bo/component/node_list_recent.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Copyright (c) 2014 Laposa Ltd (http://laposa.co.uk)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ *
+ */
+
+class Onxshop_Controller_Bo_Component_Node_List_Recent extends Onxshop_Controller {
+
+	/**
+	 * main action
+	 */
+	 
+	public function mainAction() {
+	
+		require_once('models/common/common_node.php');
+		
+		$Node = new common_node();
+		
+		$node_list = $Node->listing('', 'modified DESC', '0,20');
+				
+		foreach ($node_list as $item) {
+		
+			if ($item['publish'] == 0)  $item['class'] = 'disabled';
+			$this->tpl->assign("ITEM", $item);
+			$this->tpl->parse('content.item');
+			
+		}
+		
+		return true;
+	}
+}
