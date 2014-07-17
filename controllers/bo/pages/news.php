c9dc31ab7f5a35d89867527475c6c93e351fecfc
Hugo Dvorak <hugo@dvorak.io>
2014-07-17 17:25:38 +0100

    fix bug with adding new posts

--- controllers/bo/pages/news.php
+++ controllers/bo/pages/news.php
@@ -14,9 +14,27 @@ class Onxshop_Controller_Bo_Pages_News extends Onxshop_Controller {
 	 */
 	 
 	public function mainAction() {
-		//$id = 4;
-		//$_Onxshop_Request = new Onxshop_Request("component/filter~node_id=$id~");
-		//$this->tpl->assign("FILTER", $_Onxshop_Request->getContent());
+
+		/**
+		 * initialise
+		 */
+		 
+		require_once('models/common/common_node.php');
+		$this->Node = new common_node();
+
+		/**
+		 * basic input data
+		 */
+		 
+		if (is_numeric($this->GET['blog_node_id'])) $blog_node_id = $this->GET['blog_node_id'];
+		else $blog_node_id = $this->Node->conf['id_map-blog'];
+
+		/**
+		 * get detail of blog container node
+		 */
+		 		
+		$news_list_detail = $this->Node->getDetail($blog_node_id);
+		$this->tpl->assign('NEWS_LIST', $news_list_detail);
 
 		return true;
 	}
