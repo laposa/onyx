a50ed1c89906ab0888b64d16092d3483b1e11afb
Norbert Laposa <norbert@laposa.co.uk>
2014-07-31 16:48:07 +0100

    new age_gate component

---
+++ controllers/component/age_gate.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright (c) 2014 Laposa Ltd (http://laposa.co.uk)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ * 
+ */
+
+class Onxshop_Controller_Component_Age_Gate extends Onxshop_Controller {
+	
+	/**
+	 * main action
+	 */
+	 
+	public function mainAction() {
+
+		if (is_numeric($this->GET['min_age'])) $min_age = $this->GET['min_age'];
+		else $min_age = 18;
+
+		$birthday = array(
+			'year' => date("Y"),
+			'month' => date("m"),
+			'day' => date("d")
+		);
+
+		if ($_SESSION['client']['customer']['birthday']) {
+			$time = strtotime($_SESSION['client']['customer']['birthday']);
+			if (is_numeric($time)) {
+				$birthday = array(
+					'year' => date("Y", $time),
+					'month' => date("m", $time) - 1,
+					'day' => date("d", $time)
+				);
+			}
+		}
+		
+		$this->tpl->assign('BIRTHDAY', $birthday);
+		$this->tpl->assign('MIN_AGE', $min_age);
+
+		return true;
+	
+	}
+
+}
+
