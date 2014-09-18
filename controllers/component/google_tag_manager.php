0a628b90fa75198a45127f25042c0c996bed8a55
Hugo Dvorak <hugo@dvorak.io>
2014-09-18 13:52:05 +0100

    implement google tag manager component

---
+++ controllers/component/google_tag_manager.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Copyright (c) 2014 Laposa Ltd (http://laposa.co.uk)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ */
+
+class Onxshop_Controller_Component_Google_Tag_Manager extends Onxshop_Controller {
+	
+	/**
+	 * main action
+	 */
+	 
+	public function mainAction() {
+		
+		if (trim($GLOBALS['onxshop_conf']['global']['google_tag_manager']) != '') {
+
+			$this->tpl->parse('content.google_tag_manager');
+
+		}
+
+		return true;
+	}
+}
