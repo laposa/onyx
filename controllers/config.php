568c91d967c3264423dba351033a982519754d38
Hugo Dvorak <hugo@dvorak.io>
2014-09-18 18:13:03 +0100

    create a component to allow specific node config pre-loading for ajax calls

---
+++ controllers/config.php
@@ -0,0 +1,28 @@
+<?php
+/** 
+ * Copyright (c) 2014 Laposa Ltd (http://laposa.co.uk)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ *
+ */
+
+require_once("controllers/node.php");
+
+class Onxshop_Controller_Config extends Onxshop_Controller_Node {
+
+	/**
+	 * main action
+	 */
+	 
+	public function mainAction() {
+
+		$node_id = $this->GET['node_id'];
+
+		if (is_numeric($node_id)) {
+			$global_conf_node_overwrites = $this->initGlobalNodeConfigurationOverwrites($node_id);
+			$GLOBALS['onxshop_conf'] = $this->array_replace_recursive($GLOBALS['onxshop_conf'], $global_conf_node_overwrites);
+		}
+
+		return true;
+	}
+	
+}
