660803d66612ef66dc3f8a8574ebae33f3890f50
Norbert Laposa <norbert@laposa.co.uk>
2014-10-01 14:25:26 +0100

    add helper CSS class 'shared'

--- controllers/component/ecommerce/basket_edit.php
+++ controllers/component/ecommerce/basket_edit.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * Copyright (c) 2005-2013 Laposa Ltd (http://laposa.co.uk)
+ * Copyright (c) 2005-2014 Laposa Ltd (http://laposa.co.uk)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  * 
  */
@@ -8,5 +8,22 @@
 require_once('controllers/component/ecommerce/basket.php');
 
 class Onxshop_Controller_Component_Ecommerce_Basket_Edit extends Onxshop_Controller_Component_Ecommerce_Basket {
+
+	/**
+	 * main action
+	 */
+	 
+	public function mainAction() {
 	
+		if ($this->GET['id'] && $this->GET['code']) {
+		
+			// if basket was shared associate helper CSS class
+			$this->tpl->assign('CSS_CLASS_SHARED', 'shared');
+		
+		}
+				
+		return parent::mainAction();
+		
+	}
+		
 }
