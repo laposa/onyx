laposa/onyx

commit 2fac9fb683f610f0526c8287011e994869b48f91

fix problem with product sorting not being stored after page reload

controllers/component/ecommerce/product_list.php
@@ -515,6 +515,10 @@ class Onxshop_Controller_Component_Ecommerce_Product_List extends Onxshop_Contro
 			//default
 			$sortby = $GLOBALS['onxshop_conf']['global']['product_list_sorting'];//set in global configuration
 		
+		} else {
+
+			return;
+
 		}
 		
 		/**
@@ -530,6 +534,10 @@ class Onxshop_Controller_Component_Ecommerce_Product_List extends Onxshop_Contro
 			//default
 			$direction = 'DESC';
 		
+		} else {
+
+			return;
+
 		}
 		
 		/**

lib/onxshop.bootstrap.php
@@ -361,7 +361,7 @@ class Onxshop_Bootstrap {
 		 
 		if (is_numeric($_SESSION['use_page_cache']) && !$disable_page_cache && ONXSHOP_PAGE_CACHE_TTL > 0) $this->processActionCached($request);
 		else $this->processAction($request);
-		
+
 	}
 	
 	/**

commit 0cd2fee2a67570fd00194a9b2d8a67fe2e30c24b

fix regression caused by 2fac9fb6 - product highlights no longer sorted as listed

controllers/component/ecommerce/product_list.php
@@ -157,7 +157,15 @@ class Onxshop_Controller_Component_Ecommerce_Product_List extends Onxshop_Contro
 	
 		
 		if ($this->GET['display_sorting']) {
+
 			$this->_displaySorting();
+
+		} else if (is_array($this->GET['product_id_list']) && $this->GET['product_id_list_force_sorting_as_listed']) {
+
+			// force sorting by priority if product_id_list_force_sorting_as_listed
+			$sortby = 'priority'; //priority should have been modified to reflect product_list_list order
+			$direction = 'DESC';
+
 		}
 		
 		$product_list = $this->_processSorting($product_list, $sortby, $direction);
@@ -492,17 +500,6 @@ class Onxshop_Controller_Component_Ecommerce_Product_List extends Onxshop_Contro
 	function _prepareSorting() {
 		
 		/**
-		 * force sorting by priority if product_id_list_force_sorting_as_listed
-		 */
-		 
-		if (is_array($this->GET['product_id_list']) && $this->GET['product_id_list_force_sorting_as_listed']) {
-		
-			$sortby = 'priority'; //priority will be modified to reflect product_list_list order
-			$direction = 'DESC';
-		
-		}
-		
-		/**
 		 * sort[by]
 		 */
 		  
@@ -536,7 +533,7 @@ class Onxshop_Controller_Component_Ecommerce_Product_List extends Onxshop_Contro
 		
 		} else {
 
-			return;
+			return; // if sorting not forced by GET, then don't alter session settings
 
 		}
 		