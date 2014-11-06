112d29c66d16ee072ce8ea63881b55026a2bdcdf
Norbert Laposa <norbert@laposa.co.uk>
2014-11-06 01:11:07 +0000

    simplified backoffice add new user form

---
+++ controllers/bo/component/client/customer_add.php
@@ -0,0 +1,36 @@
+<?php
+/** 
+ * Copyright (c) 2014 Laposa Ltd (http://laposa.co.uk)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ * 
+ */
+
+class Onxshop_Controller_Bo_Component_Client_Customer_Add extends Onxshop_Controller {
+
+	/**
+	 * main action
+	 */
+	 
+	public function mainAction() {
+		
+		if ($customer_data = $_POST['client']['customer']) {
+			
+			require_once 'models/client/client_customer.php';
+			$Customer = new client_customer();
+			
+			if ($id = $Customer->registerCustomer($customer_data)) {
+				
+				onxshopGoTo("/backoffice/customers/$id/detail");
+				
+			} else {
+				
+				msg("Cannot add user", 'error');
+				
+			}
+			
+		}
+
+		return true;
+		
+	}
+}
