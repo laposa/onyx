5840c6120d522934fabeb6fc8231a6e364d9a08a
Norbert Laposa <norbert@laposa.co.uk>
2014-11-10 16:56:45 +0000

    include Facebook_Auth in node/site/facebook

--- controllers/node/site/facebook.php
+++ controllers/node/site/facebook.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * Copyright (c) 2013 Laposa Ltd (http://laposa.co.uk)
+ * Copyright (c) 2013-2014 Laposa Ltd (http://laposa.co.uk)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  * 
  */
@@ -9,4 +9,16 @@ require_once('controllers/node/site/default.php');
 
 class Onxshop_Controller_Node_Site_Facebook extends Onxshop_Controller_Node_Site_Default {
 
+	/**
+	 * main action
+	 */
+	 
+	public function mainAction() {
+				
+		$Facebook_Auth = new Onxshop_Request('component/client/facebook_auth');
+		//echo $Facebook_Auth->getContent();
+		
+		return parent::mainAction();
+		
+	}
 }
