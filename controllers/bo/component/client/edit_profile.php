1686aa166a18e13164f98a9fae091b77da15e88a
Norbert Laposa <norbert@laposa.co.uk>
2015-01-12 16:57:24 +0000

    add /backoffice/my section allowing to change currently logged in user password without further permissions

---
+++ controllers/bo/component/client/edit_profile.php
@@ -0,0 +1,71 @@
+<?php
+/** 
+ * Copyright (c) 2015 Laposa Ltd (http://laposa.co.uk)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ * 
+ */
+
+require_once('controllers/bo/component/client/edit.php');
+
+class Onxshop_Controller_Bo_Component_Client_Edit_Profile extends Onxshop_Controller_Bo_Component_Client_Edit {
+
+	/**
+	 * main action
+	 */
+	 
+	public function mainAction() {
+		
+		$this->Customer = new client_customer();
+		
+		$this->auth = Onxshop_Bo_Authentication::getInstance();
+		
+		$customer_id = $this->auth->getUserId();	
+		if (!is_numeric($customer_id)) {
+				return false;
+		}
+				
+		if ($_POST['save']) {
+		
+			$_POST['client']['customer']['id'] = $customer_id;
+			
+			// do not allow to set certain properties			
+			unset($_POST['client']['customer']['status']);
+			unset($_POST['client']['customer']['group_id']);
+			unset($_POST['client']['customer']['group_ids']);
+			unset($_POST['client']['customer']['role_ids']);
+			unset($_POST['client']['customer']['account_type']);
+			unset($_POST['client']['customer']['other_data']);
+			
+			/**
+			 * update profile
+			 */
+			 
+			if ($this->Customer->updateClient($_POST['client'])) {
+			
+				// update password 
+				$this->updatePassword($customer_id);
+			
+				msg("Backoffice profile successfully updated");
+			} else {
+				msg("Can't update backoffice profile", 'error');
+			}
+			
+		}
+		
+		$client_data = $this->Customer->getClientData($customer_id);
+		
+		$this->tpl->assign('CLIENT', $client_data);
+
+		/**
+		 * only users stored in client_customer table can update their profile
+		 */
+		 		
+		if ($customer_id === 0) {
+			$this->tpl->parse('content.other_auth');
+		} else {
+			$this->tpl->parse('content.form');
+		}
+
+		return true;
+	}
+}
