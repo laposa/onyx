13c543715b86a75e8ed14ba5ead52782e4c840dd
Hugo Dvorak <hugo@dvorak.io>
2015-02-12 18:20:44 +0000

    add 3-column recipe list template

---
+++ controllers/component/ecommerce/recipe_list_3columns.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright (c) 2013 Laposa Ltd (http://laposa.co.uk)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ * 
+ */
+
+require_once('controllers/component/ecommerce/recipe_list.php');
+
+class Onxshop_Controller_Component_Ecommerce_Recipe_List_3columns extends Onxshop_Controller_Component_Ecommerce_Recipe_List {
+
+	/**
+	 * Parse recipe list items
+	 */
+	public function parseItems(&$list)
+	{
+		$columnNames = array("One", "Two", "Three");
+
+		for ($j = 0; $j < 3; $j++) {
+			for ($i = $j; $i < count($list); $i += 3) {
+				$item = $list[$i];
+				$this->tpl->assign("ITEM", $item);
+				if ($item['image']['src']) $this->tpl->parse('content.column.item.image');
+				$this->tpl->parse("content.column.item");
+			}
+			$this->tpl->assign("COLUMN_NUM", $columnNames[$j]);
+			$this->tpl->parse("content.column");
+		}
+	}
+
+}
