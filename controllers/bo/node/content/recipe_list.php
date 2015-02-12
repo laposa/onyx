90824dce1f6dbe7e858cd01efb187ab7b07573ab
Hugo Dvorak <hugo@dvorak.io>
2015-02-12 17:14:46 +0000

    update bo interface to allow pagination on recipe_list content

--- controllers/bo/node/content/recipe_list.php
+++ controllers/bo/node/content/recipe_list.php
@@ -9,6 +9,20 @@ require_once('controllers/bo/node/content/default.php');
 class Onxshop_Controller_Bo_Node_Content_Recipe_List extends Onxshop_Controller_Bo_Node_Content_Default {
 
 	/**
+	 * pre action
+	 */
+
+	function pre() {
+	
+		parent::pre();
+		
+		if (!(is_numeric($_POST['node']['component']['limit']) && $_POST['node']['component']['limit'] > 0)) $_POST['node']['component']['limit'] = 10;
+		if ($_POST['node']['component']['pagination'] == 'on') $_POST['node']['component']['pagination'] = 1;
+		else $_POST['node']['component']['pagination'] = 0;
+
+	}
+
+	/**
 	 * post action
 	 */
 	 
@@ -24,6 +38,9 @@ class Onxshop_Controller_Bo_Node_Content_Recipe_List extends Onxshop_Controller_
 		
 		//sort-order
 		$this->tpl->assign("SELECTED_sort_direction_{$this->node_data['component']['sort']['direction']}", "selected='selected'");
+
+		//pagination
+		$this->node_data['component']['pagination']        = ($this->node_data['component']['pagination']) ? 'checked="checked"'      : '';
 		
 	}
 }
