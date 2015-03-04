c8269ebe185313942afd25551f6768ca74641067
Norbert Laposa <norbert@laposa.co.uk>
2015-03-04 13:34:50 +0000

    allow dashes in YouTube video ID

--- controllers/component/video.php
+++ controllers/component/video.php
@@ -23,7 +23,7 @@ class Onxshop_Controller_Component_Video extends Onxshop_Controller {
 		// detect youtube
 		preg_match("/https?:\/\/youtu.be\/([0-9a-zA-Z]+)/", $this->GET['video_url'], $matches);
 		if (isset($matches[1]) && !empty($matches[1])) $youtube_video_id = $matches[1];
-		preg_match("/https?:\/\/www.youtube.com\/watch\?v=([0-9a-zA-Z]+)/", $this->GET['video_url'], $matches);
+		preg_match("/https?:\/\/www.youtube.com\/watch\?v=([0-9a-zA-Z-]+)/", $this->GET['video_url'], $matches);
 		if (isset($matches[1]) && !empty($matches[1])) $youtube_video_id = $matches[1];
 
 		if ($vimeo_video_id) {
