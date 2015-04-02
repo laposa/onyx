c6cad905c8ac9d2ae22a7db5d88dd7c6b656347d
Norbert Laposa <norbert@laposa.co.uk>
2015-04-02 14:53:08 +0100

    small improvements in node edit

--- controllers/bo/component/node_other_data.php
+++ controllers/bo/component/node_other_data.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * Copyright (c) 2012 Laposa Ltd (http://laposa.co.uk)
+ * Copyright (c) 2012-2015 Laposa Ltd (http://laposa.co.uk)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  * 
  */
@@ -31,14 +31,32 @@ class Onxshop_Controller_Bo_Component_Node_Other_Data extends Onxshop_Controller
 		 */
 		
 		if (is_array($node_detail['other_data'])) {
+			
 			foreach ($node_detail['other_data'] as $key=>$value) {
+				
 				$note['key'] = $key;
 				$note['value'] = $value;
-				if ($note['key'] != '') {
-					$this->tpl->assign('OTHER_DATA', $note);
-					$this->tpl->parse('content.other_data.item');
+				
+				$this->tpl->assign('OTHER_DATA', $note);
+				
+				$allow_to_add_more = true; // we curently support editing only for scalar values
+				
+				if (is_array($value)) {
+					
+					$allow_to_add_more = false; // don't allow editing
+					$this->tpl->parse('content.other_data.item_noneditable');
+					
+				} else {
+					
+					if ($note['key'] != '') {
+						
+						$this->tpl->parse('content.other_data.item_editable');
+					}
 				}
+				
 			}
+			
+			if ($allow_to_add_more) $this->tpl->parse('content.add');
 			if (count($node_detail['other_data']) > 0) $this->tpl->parse('content.other_data');
 		}
 
