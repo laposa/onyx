8ae88f8f325b9d9222a89146e9fc8dda67a7f7a8
Norbert Laposa <norbert@laposa.co.uk>
2015-04-21 16:15:36 +0100

    allow to use admininer only from debugging IP

--- opt/adminer/adminer.php
+++ opt/adminer/adminer.php
@@ -1,4 +1,27 @@
 <?php
+//BEGIN ONXSHOP
+/**
+ * detect onxshop project directory
+ */
+
+$onxshop_project_dir =  preg_replace('/public_html\/opt/', '', dirname($_SERVER['SCRIPT_FILENAME']));
+
+/**
+ * include Onxshop project configuration
+ */
+
+require_once($onxshop_project_dir . 'conf/global.php');
+
+/**
+ * security check: allow to use only by debug IP
+ */
+
+if (!constant('ONXSHOP_IS_DEBUG_HOST')) {
+    echo "Sorry, you are not allowed to use this script. Add your IP address to debug host in conf/deployment.php";
+    exit;
+}
+//END ONXSHOP
+
 /** Adminer - Compact database management
 * @link http://www.adminer.org/
 * @author Jakub Vrana, http://www.vrana.cz/
