8f660472a8180ae1557f365b538d2259df9cb4f4
Norbert Laposa <norbert@laposa.co.uk>
2015-06-24 16:59:07 +0100

    add local.js to project_skeleton

---
+++ project_skeleton/public_html/js/local.js
@@ -0,0 +1,19 @@
+/**
+ * Norbert @ Laposa Ltd, 2015/06/24
+ */
+ 
+$(function() {
+	
+	/**
+	 * show standard Onxshop messages in a popup
+	 */
+	 
+	popupMessage("div.onxshop_messages");
+	
+	/**
+	 * write your own actions below
+	 */
+	
+	/* YOUR CODE HERE */
+	
+});
