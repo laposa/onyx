a6f402bdf22bb6dd2497412c63d584a3f52fdaf8
Norbert Laposa <norbert@laposa.co.uk>
2015-07-23 15:30:49 +0100

    prevent showing 404 when switching between backend and frontend for container page

--- controllers/node/container/default.php
+++ controllers/node/container/default.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2009-2013 Onxshop Ltd (https://onxshop.com)
+ * Copyright (c) 2009-2015 Onxshop Ltd (https://onxshop.com)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  * 
  */
@@ -27,8 +27,17 @@ class Onxshop_Controller_Node_Container_Default extends Onxshop_Controller_Node 
 		 */
 		
 		$first_parent_page_id = $Node->getParentPageId($this->GET['id']);
-				
-		onxshopGoTo("page/" . $first_parent_page_id);
+		
+		if (is_numeric($first_parent_page_id) && $first_parent_page_id > 0) {
+			
+			onxshopGoTo("page/" . $first_parent_page_id);
+		
+		} else {
+			
+			// there is no parent page to this container, forward to homepage
+			onxshopGoto("page/" . $Node->conf['id_map-homepage']);
+		
+		}
 		
 		return true;
 	}
