5cc3de827177dc759d3e1cfa7b7bb6cc6b69fb68
Norbert Laposa <norbert@laposa.co.uk>
2015-10-22 15:31:23 +0100

    new passthrough_get_parameters option for pagination

--- controllers/component/pagination.php
+++ controllers/component/pagination.php
@@ -10,12 +10,18 @@ class Onxshop_Controller_Component_Pagination extends Onxshop_Controller {
 	/**
 	 * main action
 	 */
-	 
+	
 	public function mainAction() {
-
-		// input: count of all records, records per page, from which record item we display
-		// count [int], from[int], per_page[int], link[string]
-		// link [string], option_show_all [int], option_per_page [int]
+ 
+		/**
+		 * count [int] - count of all items
+		 * from[int] - from which item we display
+		 * per_page[int] - items per page
+		 * link [string] - overwrite URL
+		 * passthrough_get_parameters [int] - TODO: currently 0 by default, consider making 1 by default
+		 * option_show_all [int] - whether to display (parse) "show all" link
+		 * option_per_page [int] - whether to display (parse) dropdown for changing number of items per page
+		*/
 		
 		/**
 		 * Compulsory input variables
@@ -24,20 +30,28 @@ class Onxshop_Controller_Component_Pagination extends Onxshop_Controller {
 		$count = (int)$this->GET['count'];
 		$from = (int)$this->GET['limit_from'];
 		$per_page = (int)$this->GET['limit_per_page'];
-
-		if ($count > $per_page) {		
-
-			/**
-			 * Optional input variables
-			 */ 
-			if ($this->GET['link'])  $link = $this->GET['link'];
-			else $link = $_SERVER['REDIRECT_URL'];
-			
-			// display options
-			if (is_numeric($this->GET['option_show_all'])) $option_show_all = $this->GET['option_show_all'];
-			else $option_show_all = 1;
-			if (is_numeric($this->GET['option_per_page'])) $option_per_page = $this->GET['option_per_page'];
-			else $option_per_page = 0;
+		
+		/**
+		 * Optional input variables
+		 */ 
+		
+		if ($this->GET['link'])  $link = $this->GET['link'];
+		else $link = $_SERVER['REDIRECT_URL'];
+		
+		if (is_numeric($this->GET['passthrough_get_parameters'])) $passthrough_get_parameters = $this->GET['passthrough_get_parameters'];
+		else $passthrough_get_parameters = 0;
+		
+		// display options
+		if (is_numeric($this->GET['option_show_all'])) $option_show_all = $this->GET['option_show_all'];
+		else $option_show_all = 1;
+		if (is_numeric($this->GET['option_per_page'])) $option_per_page = $this->GET['option_per_page'];
+		else $option_per_page = 0;
+		
+		/**
+		 * process only when necessary
+		 */
+		
+		if ($count > $per_page) {
 
 			// page items		
 			$pages_total = ceil($count / $per_page);
@@ -63,41 +77,111 @@ class Onxshop_Controller_Component_Pagination extends Onxshop_Controller {
 					'page' => $i,
 					'link' => $link
 				);
-
+				
+				if ($passthrough_get_parameters) $limit = $this->appendGetParameters($limit);
+				
 				$this->tpl->assign('LIMIT', $limit);
 				$this->tpl->assign('ITEM', $item);
 				$this->tpl->parse('content.pagination.item');
 			}
 
-			// previous
+			/**
+			 * previous
+			 */
+			 
 			if ($from > 0) {
+				
 				$previous = max($from - $per_page, 0);
+				
 				if ($previous == 0) $limit = '';
 				else $limit = "?limit_from={$previous}&amp;limit_per_page=$per_page";
+				
+				if ($passthrough_get_parameters) $limit = $this->appendGetParameters($limit);
+				
 				$this->tpl->assign('LIMIT', $limit);
 				$this->tpl->parse('content.pagination.previous');
 			}
 			
-			// next
+			/**
+			 * next
+			 */
+			 
 			$next = $from + $per_page;
+			
 			if ($next < $count)  {
+				
 				$limit = "?limit_from={$next}&amp;limit_per_page=$per_page";
+				
+				if ($passthrough_get_parameters) $limit = $this->appendGetParameters($limit);
+				
 				$this->tpl->assign('LIMIT', $limit);
 				$this->tpl->parse('content.pagination.next');
 			}
 			
-			// display options?
-			if ($option_show_all) $this->tpl->parse('content.pagination.show_all');
+			/**
+			 * display options?
+			 */
+			 
+			if ($option_show_all) {
+				
+				$limit = "?limit_from=0&limit_per_page=$count";
+				
+				if ($passthrough_get_parameters) $limit = $this->appendGetParameters($limit);
+				
+				$this->tpl->assign('LIMIT', $limit);
+				$this->tpl->parse('content.pagination.show_all');
+			}
+			
 			if ($option_per_page) {
 				$this->tpl->assign("PAGINATION_selected_$per_page", 'selected="selected"');
 				$this->tpl->parse('content.pagination.per_page');
 			}
 			
 			// parse template
-			 $this->tpl->parse('content.pagination');
+			$this->tpl->parse('content.pagination');
 
 		}
 
 		return true;
+	}
+	
+	/**
+	 * appendGetParameters
+	 */
+	
+	public function appendGetParameters($limit = '') {
+		
+		if (!property_exists($this, 'params_http_query')) $this->params_http_query = $this->getParamsHttpQuery();
+		
+		if (!$this->params_http_query) return $limit;
+		
+		if ($limit == '') $limit = "?" . $this->params_http_query;
+		else $limit = $limit = $limit . '&' . $this->params_http_query;
+
+		return $limit;
+		
+		
+	}
+	
+	/**
+	 * getParamsHttpQuery
+	 */
+	 
+	private function getParamsHttpQuery() {
+		
+		/**
+		 * filter reserved keywords
+		 */
+		 
+		$params_to_append = array();
+		
+		foreach ($_GET as $k=>$v) {
+			if (!in_array($k, array('request', 'translate', 'limit_from', 'limit_per_page'))) $params_to_append[$k] = $v;
+		}
+		
+		$params_http_query = http_build_query($params_to_append);
+		
+		return $params_http_query;
+		
 	}
 }
