5628fc5cba5bb245fee3438bfd8a603d07d19424
Norbert Laposa <norbert@laposa.co.uk>
2016-04-13 15:13:18 +0100

    update copyright notice years files edited in previous commit

--- controllers/component/teaser.php
+++ controllers/component/teaser.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * Copyright (c) 2015 Onxshop Ltd (https://onxshop.com)
+ * Copyright (c) 2015-2016 Onxshop Ltd (https://onxshop.com)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  * 
  */
