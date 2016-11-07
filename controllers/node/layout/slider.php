715a9bd2b5def347b3be46d2bacc614ad59885d6
Norbert Laposa <norbert@laposa.co.uk>
2016-11-07 17:14:56 +0000

    add slider layout

---
+++ controllers/node/layout/slider.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Copyright (c) 2016 Onxshop Ltd (https://onxshop.com)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ *
+ */
+
+require_once('controllers/node/layout/tabs.php');
+
+class Onxshop_Controller_Node_Layout_Slider extends Onxshop_Controller_Node_Layout_Tabs {
+
+}
