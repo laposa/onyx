111a9f3d07fc6d3dcd9256dcb68a0377bac7c939
Norbert Laposa <norbert@laposa.co.uk>
2016-11-18 12:45:21 +0000

    refactored onxshop shell script with implementation of certbot (Let's Encrypt)

--- utils/onxshop.sh
+++ utils/onxshop.sh
@@ -1,167 +1,210 @@
 #!/bin/bash -e
-# Norbert @ Laposa Ltd, 2012-2016
-# Very simple Onxshop websites management script
 
-####################
+## Onxshop.sh
+## Norbert @ Laposa Ltd, 2012-2016
+##
+## Very simple Onxshop websites management script.
+## Creates Onxshop installation and vhost file depending on required hostname.
+## Usage:
+##
+##     @script.name ACTION HOSTNAME [options]
+##
+## Example: You want your website to be served from http://example.com
+## Then run:
+## sudo onxshop create example.com
+##
+##
+## Options:
+##     -h, --help                        All client scripts have this, it can be omitted.
+##         --db-template-file=VALUE      Database template
+##         --project-skeleton-dir=VALUE  Project skeleton to be used
+##         --project-dir=VALUE           Folder where will be application created
+##         --db-username=VALUE           Database username to be used or created if doesn't exists
+##         --db-password=VALUE           Database password to be used
+##         --vhost                       Create Apache vhost file and enable it
+##         --ssl                         Create SSL certificate (Let's Encrypt via Certbot)
+
+script_dir=$(dirname "$BASH_SOURCE")
+source "${script_dir}/easyoptions.sh" || exit
+
+# Arguments
+#for argument in "${arguments[@]}"; do
+#    echo "Argument specified: $argument"
+#done
+
+# Boolean and parameter options
+#[[ -n "$db_template_file"  ]] && echo "Option specified: --db-template-file is $db_template_file"
+#[[ -n "$project_skeleton_dir"  ]] && echo "Option specified: --project-skeleton-dir is $project_skeleton_dir"
+#[[ -n "$project_dir"  ]] && echo "Option specified: --project-dir is $project_dir"
+#[[ -n "$db_username"  ]] && echo "Option specified: --db-username is $db_username"
+#[[ -n "$db_password"  ]] && echo "Option specified: --db-password is $db_password"
+#[[ -n "$vhost"  ]] && echo "Option specified: --vhost"
+#[[ -n "$ssl"  ]] && echo "Option specified: --ssl"
+
 # input parameters #
-####################
 
-ACTION=$1 # mandatory
-HOSTNAME=$2 # mandatory
-DB_TEMPLATE_FILE=$3 # optional
-PROJECT_SKELETON_DIR=$4 # optional
-PROJECT_DIR=$5 # optional
-DB_USERNAME=$6 # optional
-#DB_PASSWORD=$7 # optional
+action=${arguments[0]} # mandatory
+hostname=${arguments[1]} # mandatory
 
-#####################
 # prepare functions #
-#####################
-
-usage() {
-cat <<EOF
-$0 ACTION HOSTNAME [DB_TEMPLATE_FILE] [PROJECT_SKELETON_DIR] [PROJECT_DIR] [DB_USERNAME]
-
-Creates Onxshop installation and vhost file depending on required hostname.
-
-Example: You want your website to be served from http://example.com
-Then run:
-sudo onxshop create example.com
-
-EOF
-}
 
 setup_variables() {
-ONXSHOP_VERSION="1.7"
-ONXSHOP_VERSION_DB=$(echo $ONXSHOP_VERSION | sed 's,\.,_,g')
-if ! [ $DB_USERNAME ]; then
+onxshop_version="1.7"
+onxshop_version_db=$(echo $onxshop_version | sed 's,\.,_,g')
+if ! [ $db_username ]; then
 	determine_username_from_domainname
 fi
-if ! [ $PROJECT_DIR ]; then
-	PROJECT_DIR="/srv/$HOSTNAME"
+if ! [ $project_dir ]; then
+	project_dir="/srv/$hostname"
 fi
-if ! [ $DB_TEMPLATE_FILE ]; then
-	DB_TEMPLATE_FILE=/opt/onxshop/${ONXSHOP_VERSION}/project_skeleton/base_with_blog.sql
+if ! [ $db_template_file ]; then
+	db_template_file=/opt/onxshop/${onxshop_version}/project_skeleton/base_with_blog.sql
 fi
-if ! [ $PROJECT_SKELETON_DIR ]; then
-	PROJECT_SKELETON_DIR=/opt/onxshop/$ONXSHOP_VERSION/project_skeleton/base_with_blog/
+if ! [ $project_skeleton_dir ]; then
+	project_skeleton_dir=/opt/onxshop/$onxshop_version/project_skeleton/base_with_blog/
 fi
 
 echo "
 Variables are set to:
 
-ACTION=$ACTION
-HOSTNAME=$HOSTNAME
-DB_TEMPLATE_FILE=$DB_TEMPLATE_FILE
-PROJECT_SKELETON_DIR=$PROJECT_SKELETON_DIR
-PROJECT_DIR=$PROJECT_DIR
-DB_USERNAME=$DB_USERNAME
+action=$action
+hostname=$hostname
+db_template_file=$db_template_file
+project_skeleton_dir=$project_skeleton_dir
+project_dir=$project_dir
+db_username=$db_username
 "
 
 }
 
 test_input() {
-if ! [ $ACTION ]; then
-	die "ACTION not provided"
+if ! [ $action ]; then
+	die "action not provided"
 fi
 
-if ! [ $HOSTNAME ]; then
-	die "HOSTNAME not provided"
+if ! [ $hostname ]; then
+	die "hostname not provided"
 fi
 }
 
 create_new_installation() {
-	random_password
+	get_password
 	copy_files
 	setup_database
 	change_config
-#	create_vhost
+	if [ -n "$vhost"  ] ; then
+		create_vhost
+	fi
 	show_result
 }
 
 # Universal function for bailing out
-die() { 
-usage
+die() {
+$0 -h
 echo -e "*** $1\n*** See https//onxshop.com/"; exit 1; 
+}
+
+get_password() {
+if ! [ $db_password ]; then
+	random_password
+fi
 }
 
 random_password() {
 # using pwgen if installed
-# DB_PASSWORD="`pwgen -N 1`"
+# db_password="`pwgen -N 1`"
 
 # Generate a random password without Perl
 MATRIX="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 LENGTH="8"
 while [ "${n:=1}" -le "$LENGTH" ]
 do
-    DB_PASSWORD="$DB_PASSWORD${MATRIX:$(($RANDOM%${#MATRIX})):1}"
+    db_password="$db_password${MATRIX:$(($RANDOM%${#MATRIX})):1}"
 	let n+=1
 done
 }
 
 copy_files() {
-if [ -d $PROJECT_DIR ]; then
-	die "Directory $PROJECT_DIR already exists"
+if [ -d $project_dir ]; then
+	die "Directory $project_dir already exists"
 fi
-cp -a $PROJECT_SKELETON_DIR $PROJECT_DIR
-rm $PROJECT_DIR/onxshop_dir && ln -s /opt/onxshop/$ONXSHOP_VERSION/ $PROJECT_DIR/onxshop_dir
-chmod a+w -R $PROJECT_DIR/var/
+cp -a $project_skeleton_dir $project_dir
+rm $project_dir/onxshop_dir && ln -s /opt/onxshop/$onxshop_version/ $project_dir/onxshop_dir
+chmod a+w -R $project_dir/var/
 }
 
 setup_database() {
-DB_NAME="${DB_USERNAME}-${ONXSHOP_VERSION_DB}"
+db_name="${db_username}-${onxshop_version_db}"
 # TODO create user only if doesn't exist
-sudo -u postgres psql template1 -c "CREATE USER $DB_USERNAME WITH CREATEDB NOCREATEUSER PASSWORD '$DB_PASSWORD'"
-sudo -u postgres psql template1 -c "CREATE DATABASE \"$DB_NAME\" WITH OWNER=\"$DB_USERNAME\" ENCODING='UTF8'"
-export PGPASSWORD=${DB_PASSWORD}
-psql -U ${DB_USERNAME} -h localhost $DB_NAME < $DB_TEMPLATE_FILE 
-psql -U ${DB_USERNAME} -h localhost $DB_NAME -c "UPDATE common_configuration SET value='$DB_USERNAME' WHERE property='title'";
+sudo -u postgres psql template1 -c "CREATE USER $db_username WITH CREATEDB NOCREATEUSER PASSWORD '$db_password'"
+sudo -u postgres psql template1 -c "CREATE DATABASE \"$db_name\" WITH OWNER=\"$db_username\" ENCODING='UTF8'"
+export PGPASSWORD=${db_password}
+psql -U ${db_username} -h localhost $db_name < $db_template_file 
+psql -U ${db_username} -h localhost $db_name -c "UPDATE common_configuration SET value='$db_username' WHERE property='title'";
 }
 
 change_config() {
-DEPLOYMENT_FILE="$PROJECT_DIR/conf/deployment.php"
-sed -i "s/define('ONXSHOP_DB_USER', '.*')/define('ONXSHOP_DB_USER', '$DB_USERNAME')/g" $DEPLOYMENT_FILE
-sed -i "s/define('ONXSHOP_DB_PASSWORD', '.*')/define('ONXSHOP_DB_PASSWORD', '$DB_PASSWORD')/g" $DEPLOYMENT_FILE
-sed -i "s/define('ONXSHOP_DB_NAME', '.*')/define('ONXSHOP_DB_NAME', '$DB_NAME')/g" $DEPLOYMENT_FILE
+deployment_file="$project_dir/conf/deployment.php"
+sed -i "s/define('ONXSHOP_DB_USER', '.*')/define('ONXSHOP_DB_USER', '$db_username')/g" $deployment_file
+sed -i "s/define('ONXSHOP_DB_PASSWORD', '.*')/define('ONXSHOP_DB_PASSWORD', '$db_password')/g" $deployment_file
+sed -i "s/define('ONXSHOP_DB_NAME', '.*')/define('ONXSHOP_DB_NAME', '$db_name')/g" $deployment_file
 }
 
 create_vhost() {
-VHOST_FILE="/etc/apache2/sites-available/${HOSTNAME}.conf"
-if [ -f ${VHOST_FILE} ]; then
-    die "Vhost file ${VHOST_FILE} already exists"
+vhost_file="/etc/apache2/sites-available/${hostname}.conf"
+if [ -f ${vhost_file} ]; then
+    die "Vhost file ${vhost_file} already exists"
 fi
 echo "<VirtualHost *:80>
-	ServerName ${HOSTNAME}
-	VirtualDocumentRoot ${PROJECT_DIR}/public_html
-</VirtualHost>" > ${VHOST_FILE} || die "Couldn't add vhost file"
-a2ensite ${HOSTNAME} && service apache2 reload
+	ServerName ${hostname}
+	VirtualDocumentRoot ${project_dir}/public_html
+</VirtualHost>" > ${vhost_file} || die "Couldn't add vhost file ${vhost_file}"
+
+a2ensite ${hostname} && service apache2 reload
+
+if [ -n "$ssl"  ] ; then
+	if which certbot >/dev/null; then
+    	echo 'found certbot, executing'
+    	certbot --apache --redirect --non-interactive --text -d ${hostname} 2>&1
+	else
+		echo "Couldn't create SSL certificate, please insteall Certbot"
+	fi
+fi
+
 }
 
 test_hostname_is_valid() {
-if [ $HOSTNAME ] ; then
-ping -c 1 `echo $HOSTNAME | sed 's/:[0-9]\+//'` || exit 1
+if [ $hostname ] ; then
+ping -c 1 `echo $hostname | sed 's/:[0-9]\+//'` || exit 1
 else
-   die "provide domain name"
+   die "provide a valid hostname"
    exit 0
 fi
 }
 
 determine_username_from_domainname() {
-DB_USERNAME=$(echo $HOSTNAME | sed 's,\.,,g;s,-,,g')
-echo Constructed database name and user: $DB_USERNAME from $HOSTNAME
+db_username=$(echo $hostname | sed 's,\.,,g;s,-,,g')
+echo Constructed database name and user: $db_username from $hostname
 }
 
 show_result() {
-echo "Your Onxshop website is installed in: $PROJECT_DIR
+
+if [ -n "$ssl"  ] ; then 
+	protocol='https';
+else 
+	protocol='http';
+fi
+
+echo "Your Onxshop website is installed in: $project_dir
 To edit the website use
-URL: https://${HOSTNAME}/edit
-Username: ${DB_USERNAME}
-Password: ${DB_PASSWORD}
+URL: ${protocol}://${hostname}/edit
+Username: ${db_username}
+Password: ${db_password}
 "
 }
 
 process_action() {
-case "$ACTION" in
+case "$action" in
   create)
     create_new_installation
     ;;
@@ -169,17 +212,15 @@ case "$ACTION" in
     die "not implemented"
     ;;
   '')
-    die "Sorry, no ACTION provided"
+    die "Sorry, no action provided"
     ;;
   *)
-    die "Sorry, unknown ACTION"
+    die "Sorry, unknown action"
     ;;
 esac
 }
 
-##########################
-## start procedure here ##
-##########################
+# start procedure here ##
 test_input
 setup_variables
 #test_hostname_is_valid
