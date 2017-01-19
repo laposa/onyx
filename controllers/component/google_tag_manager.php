89076ff30b1c84018cdd7759387c12072628cdbc
Norbert Laposa <norbert@laposa.co.uk>
2017-01-19 16:39:34 +0000

    separate Google Tag Manager code into HEAD and BODY as per recommendation

--- controllers/component/google_tag_manager.php
+++ controllers/component/google_tag_manager.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2014 Onxshop Ltd (https://onxshop.com)
+ * Copyright (c) 2014-2017 Onxshop Ltd (https://onxshop.com)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  */
 
@@ -14,7 +14,8 @@ class Onxshop_Controller_Component_Google_Tag_Manager extends Onxshop_Controller
 		
 		if (trim($GLOBALS['onxshop_conf']['global']['google_tag_manager']) != '') {
 
-			$this->tpl->parse('content.google_tag_manager');
+			$this->tpl->parse('head.gtm');
+			$this->tpl->parse('content.gtm');
 
 		}
 
