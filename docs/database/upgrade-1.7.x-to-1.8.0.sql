0d307b25465ce2a7e05b0afa830ca9c68a8ec82a
Norbert Laposa <norbert@laposa.co.uk>
2017-05-29 10:52:06 +0100

    add update from previous commit also to 1.8 upgrade script

--- docs/database/upgrade-1.7.x-to-1.8.0.sql
+++ docs/database/upgrade-1.7.x-to-1.8.0.sql
@@ -14,4 +14,13 @@ UPDATE common_node SET node_controller = 'image_gallery' WHERE node_controller =
 
 ALTER TABLE common_node RENAME COLUMN teaser TO strapline;
 
+DELETE FROM ecommerce_transaction
+WHERE id IN (SELECT id
+              FROM (SELECT id,
+                             ROW_NUMBER() OVER (partition BY order_id, pg_data ORDER BY id) AS rnum
+                     FROM ecommerce_transaction) t
+              WHERE t.rnum > 1);
+
+ALTER TABLE ONLY ecommerce_transaction ADD CONSTRAINT ecommerce_transaction_order_id_pg_data_key UNIQUE (order_id, pg_data);
+
 COMMIT;
