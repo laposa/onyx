db81e479b8866949836cea4178713ce7773562d0
Hugo Dvorak <hugo@dvorak.io>
2017-06-08 19:33:18 +0100

    update rsync script to allow syncing only specific files or subdirectories

--- utils/rsync.sh
+++ utils/rsync.sh
@@ -1,41 +1,52 @@
 #!/bin/bash
 # Norbert Laposa, 2007
-# Hugo Dvorak, 2014
+# Hugo Dvorak, 2014-2017
 
-#get user input
-CONFIRM="${1}";
+# help
+echo
+echo "Usage: ${0} [YES] [file/dir (use to sync only a specific file or subdirectory)]"
+echo
 
-#setting local environment
-PROJECT_LOCAL_PATH="./";
+# arguments
+CONFIRM="${1}"
+FILE=""
 
-#setting remote environment
+# allow syncing only specific file/subdir
+if [ $# -eq 2 ]; then
+	FILE="${2}"
+	if [ ! -f $FILE ] && [ ! -d $FILE ] ; then
+	    echo "$FILE is an invalid path"
+	    exit 1
+	fi
+fi
+
+# setting local environment
+PROJECT_LOCAL_PATH="./${FILE}"
+
+# setting remote environment
 PROJECT_REMOTE_USERNAME="username";
 PROJECT_REMOTE_SERVER="example.com";
 PROJECT_REMOTE_BASE_DIR="/home/example/example.com/";
-PROJECT_REMOTE_FULL_PATH="${PROJECT_REMOTE_USERNAME}@${PROJECT_REMOTE_SERVER}:${PROJECT_REMOTE_BASE_DIR}";
-PROJECT_REMOTE_FULL_PUBLIC_DIR="${PROJECT_REMOTE_FULL_PATH}";
+PROJECT_REMOTE_FULL_PATH="${PROJECT_REMOTE_USERNAME}@${PROJECT_REMOTE_SERVER}:${PROJECT_REMOTE_BASE_DIR}${FILE}"
+PROJECT_REMOTE_FULL_PUBLIC_DIR="${PROJECT_REMOTE_FULL_PATH}"
 
-#backup
-DATE=`date -u +%F_%H%M%S`;
-BACKUP_PATH="${PROJECT_REMOTE_BASE_DIR}_resources/backup/${DATE}";
+# backup
+DATE=`date -u +%F_%H%M%S`
+BACKUP_PATH="${PROJECT_REMOTE_BASE_DIR}_resources/backup/${DATE}"
 
-#help
-echo
-echo "Usage: ${0} [YES]";
-echo
-
+# dry run?
 if [ "$CONFIRM" != "YES" ]; then
-	echo "Add parameter YES, if you want to write changes.";
+	echo "Add parameter YES, if you want to write changes."
 	echo
-	echo "Target path: $PROJECT_REMOTE_FULL_PUBLIC_DIR";
-	NOPROCESS="-n";
+	echo "Target path: $PROJECT_REMOTE_FULL_PUBLIC_DIR"
+	NOPROCESS="-n"
 else
-	echo "Syncing $PROJECT_LOCAL_PATH with $PROJECT_REMOTE_FULL_PUBLIC_DIR";
-	echo "Backup in $BACKUP_PATH";
-fi;
+	echo "Syncing $PROJECT_LOCAL_PATH with $PROJECT_REMOTE_FULL_PUBLIC_DIR"
+	echo "Backup in $BACKUP_PATH"
+fi
 echo
 
-#process rsync
+# process rsync
 rsync --recursive --backup --backup-dir=$BACKUP_PATH --times --perms -e 'ssh -p 22' --links --safe-links --delete-after --compress --progress --whole-file ${NOPROCESS}\
 	--exclude '*.marks' \
 	--exclude '._*' \
@@ -56,4 +67,3 @@ rsync --recursive --backup --backup-dir=$BACKUP_PATH --times --perms -e 'ssh -p 
 	--exclude '.gitignore' \
 	$PROJECT_LOCAL_PATH \
 	$PROJECT_REMOTE_FULL_PUBLIC_DIR
-
