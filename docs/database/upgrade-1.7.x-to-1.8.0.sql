101514b804797eb5997b8cae93fc3616ad1716f2
Norbert Laposa <norbert@laposa.co.uk>
2017-06-27 16:19:10 +0100

    update all database templates to v1.8

--- docs/database/upgrade-1.7.x-to-1.8.0.sql
+++ docs/database/upgrade-1.7.x-to-1.8.0.sql
@@ -1,9 +1,5 @@
 BEGIN;
 
-ALTER TABLE client_customer ALTER facebook_id TYPE character varying(255);
-ALTER TABLE client_customer ALTER twitter_id TYPE character varying(255);
-ALTER TABLE client_customer ALTER google_id TYPE character varying(255);
-
 ALTER TABLE common_node ADD COLUMN custom_fields jsonb;
 
 CREATE INDEX common_node_custom_fields_idx ON common_node USING gin (custom_fields);
@@ -13,14 +9,5 @@ UPDATE common_node SET node_controller = lower(node_controller);
 UPDATE common_node SET node_controller = 'image_gallery' WHERE node_controller = 'picture' AND node_group='content';
 
 ALTER TABLE common_node RENAME COLUMN teaser TO strapline;
-
-DELETE FROM ecommerce_transaction
-WHERE id IN (SELECT id
-              FROM (SELECT id,
-                             ROW_NUMBER() OVER (partition BY order_id, pg_data ORDER BY id) AS rnum
-                     FROM ecommerce_transaction) t
-              WHERE t.rnum > 1);
-
-ALTER TABLE ONLY ecommerce_transaction ADD CONSTRAINT ecommerce_transaction_order_id_pg_data_key UNIQUE (order_id, pg_data);
 
 COMMIT;
