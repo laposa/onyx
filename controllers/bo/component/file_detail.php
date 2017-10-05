01abbbe6c09ab535ca6e3c104ba768ec8d91a223
Norbert Laposa <norbert@laposa.co.uk>
2017-10-05 13:45:32 +0100

    remove duplicated default option

--- controllers/bo/component/file_detail.php
+++ controllers/bo/component/file_detail.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * Copyright (c) 2010-2011 Onxshop Ltd (https://onxshop.com)
+ * Copyright (c) 2010-2017 Onxshop Ltd (https://onxshop.com)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  */
 
@@ -8,50 +8,49 @@ require_once('controllers/bo/component/file.php');
 
 class Onxshop_Controller_Bo_Component_File_Detail extends Onxshop_Controller_Bo_Component_File {
 
-	/**
-	 * main action
-	 */
-	 
-	public function mainAction() {
-	
-		$file_id = $this->GET['file_id'];
-		$type = $this->GET['type'];
-		$relation = $this->GET['relation'];
-		
-		$File = $this->initializeFile($relation);
+    /**
+     * main action
+     */
+     
+    public function mainAction() {
+    
+        $file_id = $this->GET['file_id'];
+        $type = $this->GET['type'];
+        $relation = $this->GET['relation'];
+        
+        $File = $this->initializeFile($relation);
 
-		$this->tpl->assign('IMAGE_CONF', $File->conf);
-		
-		if ($file_detail = $File->getFileDetail($file_id)) {
-			
-			$this->tpl->assign('FILE', $file_detail);
+        $this->tpl->assign('IMAGE_CONF', $File->conf);
+        
+        if ($file_detail = $File->getFileDetail($file_id)) {
+            
+            $this->tpl->assign('FILE', $file_detail);
 
-			switch ($type) {
-			
-				case'RTE':
-					$this->tpl->parse("content.image_zoom");
-					$this->tpl->parse("content.image_view_full");
-					$this->tpl->parse("content.RTE_select");
-				break;
-				case 'CSS':
-					$this->tpl->parse("content.image_zoom");
-					$this->tpl->parse("content.image_view_full");
-					$this->tpl->parse("content.CSS_select");
-				break;
-				default:
-				case 'file':
-					//nothing
-				break;
-				default:
-					$this->tpl->parse("content.image_zoom");
-					$this->tpl->parse("content.image_view_full");
-					$this->tpl->parse("content.default");
-				break;
-			}
-					
-		}
+            switch ($type) {
+            
+                case'RTE':
+                    $this->tpl->parse("content.image_zoom");
+                    $this->tpl->parse("content.image_view_full");
+                    $this->tpl->parse("content.RTE_select");
+                break;
+                case 'CSS':
+                    $this->tpl->parse("content.image_zoom");
+                    $this->tpl->parse("content.image_view_full");
+                    $this->tpl->parse("content.CSS_select");
+                break;
+                case 'file':
+                    //nothing
+                break;
+                default:
+                    $this->tpl->parse("content.image_zoom");
+                    $this->tpl->parse("content.image_view_full");
+                    $this->tpl->parse("content.default");
+                break;
+            }
+                    
+        }
 
-		return true;
-	}
-	
+        return true;
+    }
+    
 }
--- controllers/bo/component/file_list_icon.php
+++ controllers/bo/component/file_list_icon.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * Copyright (c) 2010-2011 Onxshop Ltd (https://onxshop.com)
+ * Copyright (c) 2010-2017 Onxshop Ltd (https://onxshop.com)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  */
 
@@ -8,38 +8,37 @@ require_once('controllers/bo/component/file_list.php');
 
 class Onxshop_Controller_Bo_Component_File_List_Icon extends Onxshop_Controller_Bo_Component_File_List {
 
-	/**
-	 * parse item
-	 */
-	
-	public function parseItem($file_detail, $type, $relation) {
-					
-		$this->tpl->assign('FILE', $file_detail);
-			
-		switch ($type) {
-		
-			case'RTE':
-				$this->tpl->parse("content.list.item.image_zoom");
-				$this->tpl->parse("content.list.item.image_view_full");
-				$this->tpl->parse("content.list.item.RTE_select");
-			break;
-			case 'CSS':
-				$this->tpl->parse("content.list.item.image_zoom");
-				$this->tpl->parse("content.list.item.image_view_full");
-				$this->tpl->parse("content.list.item.CSS_select");
-			break;
-			default:
-			case 'file':
-				//nothing
-			break;
-			default:
-				$this->tpl->parse("content.list.item.image_zoom");
-				$this->tpl->parse("content.list.item.image_view_full");
-				$this->tpl->parse("content.list.item.default");
-			break;
-		}
-			
-		$this->tpl->parse("content.list.item");
-	}
-	
+    /**
+     * parse item
+     */
+    
+    public function parseItem($file_detail, $type, $relation) {
+                    
+        $this->tpl->assign('FILE', $file_detail);
+            
+        switch ($type) {
+        
+            case'RTE':
+                $this->tpl->parse("content.list.item.image_zoom");
+                $this->tpl->parse("content.list.item.image_view_full");
+                $this->tpl->parse("content.list.item.RTE_select");
+            break;
+            case 'CSS':
+                $this->tpl->parse("content.list.item.image_zoom");
+                $this->tpl->parse("content.list.item.image_view_full");
+                $this->tpl->parse("content.list.item.CSS_select");
+            break;
+            case 'file':
+                //nothing
+            break;
+            default:
+                $this->tpl->parse("content.list.item.image_zoom");
+                $this->tpl->parse("content.list.item.image_view_full");
+                $this->tpl->parse("content.list.item.default");
+            break;
+        }
+            
+        $this->tpl->parse("content.list.item");
+    }
+    
 }
