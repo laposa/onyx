3dc9fc2ce33c830c7da256764358efe2b8a0c0c4
Norbert Laposa <norbert@laposa.co.uk>
2017-10-05 16:44:53 +0100

    removed PhpPgAdmin

--- opt/phppgadmin/multiactionform.js
+++
@@ -1,9 +0,0 @@
-function checkAll(bool) {
-
-	var inputs = document.getElementById('multi_form').getElementsByTagName('input');
-
-	for (var i=0; i<inputs.length; i++) {
-		if (inputs[i].type == 'checkbox')
-			inputs[i].checked = bool;
-	}
-}
