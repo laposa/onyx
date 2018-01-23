3eff6cc660678f0b78e8b0bcf705373fc39dcbac
Norbert Laposa <norbert@laposa.co.uk>
2018-01-23 12:29:43 +0000

    add page list (teaser) shelf

---
+++ controllers/component/teaser_shelf.php
@@ -0,0 +1,12 @@
+<?php
+/** 
+ * Copyright (c) 2018 Onxshop Ltd (https://onxshop.com)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ * 
+ */
+
+require_once('controllers/component/teaser.php');
+
+class Onxshop_Controller_Component_Teaser_Shelf extends Onxshop_Controller_Component_Teaser {
+
+}
