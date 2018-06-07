f5611c0b232c210b34259136b093284225e0904d
Norbert Laposa <norbert@laposa.co.uk>
2018-06-07 14:26:44 +0100

    separate taxonomy list and taxonomy_list_image

---
+++ controllers/component/taxonomy_list_image.php
@@ -0,0 +1,12 @@
+<?php
+/** 
+ * Copyright (c) 2018 Onxshop Ltd (https://onxshop.com)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ *
+ */
+
+require_once('controllers/component/taxonomy_list.php');
+
+class Onxshop_Controller_Component_Taxonomy_List_Image extends Onxshop_Controller_Component_Taxonomy_List {
+    
+}
