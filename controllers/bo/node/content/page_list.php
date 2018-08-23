c754747a4307b8f79f1335791fc59e868b660b47
Hugo Dvorak <hugo@dvorak.io>
2018-08-23 17:54:21 +0200

    update page list content to detect existing item templates

--- controllers/bo/node/content/page_list.php
+++ controllers/bo/node/content/page_list.php
@@ -20,4 +20,43 @@ class Onxshop_Controller_Bo_Node_Content_Page_List extends Onxshop_Controller_Bo
         $this->tpl->assign("SELECTED_template_{$this->node_data['component']['template']}", "selected='selected'");
         
     }
+
+    function assign() {
+
+        parent::assign();
+
+        $this->renderTeaserTemplatesDropdown();
+
+    }
+
+    /**
+     * List existing teaser_*.html files in the template dropdown
+     */
+    function renderTeaserTemplatesDropdown() {
+        $teaser_templates = [];
+
+        $dir = new DirectoryIterator(ONXSHOP_PROJECT_DIR . "/templates/component");
+        foreach ($dir as $fileinfo) {
+            $filename = $fileinfo->getFilename();
+            if (!$fileinfo->isDot() && substr($filename, 0, 7) == "teaser_") {
+                $teaser_templates[$filename] = str_replace("teaser_", "", str_replace(".html", "", $filename));
+            }
+        }
+
+        $dir = new DirectoryIterator(ONXSHOP_DIR . "/templates/component");
+        foreach ($dir as $fileinfo) {
+            $filename = $fileinfo->getFilename();
+            if (!$fileinfo->isDot() && substr($filename, 0, 7) == "teaser_") {
+                $teaser_templates[$filename] = str_replace("teaser_", "", str_replace(".html", "", $filename));
+            }
+        }
+        foreach ($teaser_templates as $template) {
+            $selected = '';
+            if ($this->node_data['component']['template'] == $template) $selected = 'selected="selected"';
+            $this->tpl->assign("TEASER_TEMPLATE", $template);
+            $this->tpl->assign("SELECTED", $selected);
+            $this->tpl->parse("content.teaser_template_item");
+        }
+    }
+
 }
