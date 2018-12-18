337bdd515361bcb5aa9eb371c7920a543ba3fae7
Norbert Laposa <norbert@laposa.ie>
2018-12-18 19:15:42 +0000

    remove characters 0,o,i,l from password generator

--- models/client/client_customer.php
+++ models/client/client_customer.php
@@ -1205,7 +1205,7 @@ ALTER TABLE ONLY client_customer ADD CONSTRAINT client_customer_email_key UNIQUE
     
         //or use /usr/bin/pwgen?
 
-        $hash= array("1","2","3","4","5","6","7","8","9","0","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+        $hash= array("1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","j","k","m","n","p","q","r","s","t","u","v","w","x","y","z");
 
         $password="";
 
