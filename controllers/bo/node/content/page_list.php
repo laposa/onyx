69929da4db50d21c47771d2eee15ca9c5ca30813
Norbert Laposa <norbert@laposa.ie>
2019-02-01 11:30:16 +0000

    bug fixes in page lists

--- controllers/bo/node/content/page_list.php
+++ controllers/bo/node/content/page_list.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2013-2014 Onxshop Ltd (https://onxshop.com)
+ * Copyright (c) 2013-2019 Onxshop Ltd (https://onxshop.com)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  */
 
@@ -25,37 +25,38 @@ class Onxshop_Controller_Bo_Node_Content_Page_List extends Onxshop_Controller_Bo
 
         parent::assign();
 
-        $this->renderTeaserTemplatesDropdown();
+        $this->renderPageListTemplatesDropdown();
 
     }
 
     /**
-     * List existing teaser_*.html files in the template dropdown
+     * List existing page_list_*.html files in the template dropdown
      */
-    function renderTeaserTemplatesDropdown() {
-        $teaser_templates = [];
+    function renderPageListTemplatesDropdown() {
+        $page_list_templates = [];
 
         $dir = new DirectoryIterator(ONXSHOP_PROJECT_DIR . "/templates/component");
         foreach ($dir as $fileinfo) {
             $filename = $fileinfo->getFilename();
-            if (!$fileinfo->isDot() && substr($filename, 0, 7) == "teaser_") {
-                $teaser_templates[$filename] = str_replace("teaser_", "", str_replace(".html", "", $filename));
+            if (!$fileinfo->isDot() && substr($filename, 0, 10) == "page_list_") {
+                $page_list_templates[$filename] = str_replace("page_list_", "", str_replace(".html", "", $filename));
             }
         }
 
         $dir = new DirectoryIterator(ONXSHOP_DIR . "/templates/component");
         foreach ($dir as $fileinfo) {
             $filename = $fileinfo->getFilename();
-            if (!$fileinfo->isDot() && substr($filename, 0, 7) == "teaser_") {
-                $teaser_templates[$filename] = str_replace("teaser_", "", str_replace(".html", "", $filename));
+            if (!$fileinfo->isDot() && substr($filename, 0, 10) == "page_list_") {
+                $page_list_templates[$filename] = str_replace("page_list_", "", str_replace(".html", "", $filename));
             }
         }
-        foreach ($teaser_templates as $template) {
+        
+        foreach ($page_list_templates as $template) {
             $selected = '';
             if ($this->node_data['component']['template'] == $template) $selected = 'selected="selected"';
-            $this->tpl->assign("TEASER_TEMPLATE", $template);
+            $this->tpl->assign("PAGE_LIST_TEMPLATE", $template);
             $this->tpl->assign("SELECTED", $selected);
-            $this->tpl->parse("content.teaser_template_item");
+            $this->tpl->parse("content.page_list_template_item");
         }
     }
 
