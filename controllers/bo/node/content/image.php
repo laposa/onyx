9818d7ce96a0cbc6921609827afc1a58cb19c7cc
Norbert Laposa <norbert@laposa.ie>
2019-03-19 16:53:10 +0000

    implement show_caption checkbox in PHP

--- controllers/bo/node/content/image.php
+++ controllers/bo/node/content/image.php
@@ -1,12 +1,22 @@
 <?php
 /**
- * Copyright (c) 2006-2017 Onxshop Ltd (https://onxshop.com)
+ * Copyright (c) 2006-2019 Onxshop Ltd (https://onxshop.com)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  */
 
 require_once('controllers/bo/node/content/default.php');
 
 class Onxshop_Controller_Bo_Node_Content_Image extends Onxshop_Controller_Bo_Node_Content_Default {
+    /**
+     * pre action
+     */
+     
+    function pre() {
     
+      if ($_POST['node']['component']['show_caption'] == 'on' || $_POST['node']['component']['show_caption'] == 1) $_POST['component']['show_caption'] = 1;
+      else $_POST['node']['component']['show_caption'] = 0;
+      
+      return parent::pre();
+  }
 }
 
