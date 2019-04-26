87752feb84bea083a8f896c3495744cd038ce144
Norbert Laposa <norbert@laposa.ie>
2019-04-26 11:16:14 +0100

    update documentation for API

--- controllers/api/v1_4/store_location_list.php
+++ controllers/api/v1_4/store_location_list.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * Copyright (c) 2015 Onxshop Ltd (https://onxshop.com)
+ * Copyright (c) 2015-2019 Onxshop Ltd (https://onxshop.com)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  * 
  */
@@ -9,6 +9,5 @@ require_once('controllers/api/v1_3/store_location_list.php');
 
 class Onxshop_Controller_Api_v1_4_Store_Location_List extends Onxshop_Controller_Api_v1_3_Store_Location_List {
     
-    // TODO in v1.4 - start showing separete fields as in ecommerce_store.address_*
     
 }
