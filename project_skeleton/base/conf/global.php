ed780154af8d400d4d4d558801a78ac19bb53425
Norbert Laposa <norbert@laposa.ie>
2020-09-21 18:59:22 +0100

    refactoring project skeleton

--- project_skeleton/base/conf/global.php
+++ project_skeleton/base/conf/global.php
@@ -2,7 +2,7 @@
 /**
  * Global configuration
  *
- * Copyright (c) 2009-2016 Onxshop Ltd (https://onxshop.com)
+ * Copyright (c) 2009-2020 Onxshop Ltd (https://onxshop.com)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  * 
  * This is configuration shared between development and production environment
@@ -11,17 +11,60 @@
  */
 
 /**
- * Server specific configuration (i.e. different on dev&live server)
+ * Include composer packages
  */
 
-require_once('deployment.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 /**
- * Project specific configuration (i.e. shared on dev&live server)
- * See onxshop_dir/conf/global.php for all options
+ * Load env variables
  */
- 
-//define('ONXSHOP_MAIN_TEMPLATE','node/site/default');
+
+if (file_exists(__DIR__ . "/../.env")) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+    $dotenv->load();
+}
+
+/**
+ * Application debug settings
+ *
+ * 0 - No debugging
+ * 1 - Basic debugging
+ * 5 - Full debugging
+ *
+ * to enable direct debugging use:
+ * define('ONXSHOP_DEBUG_DIRECT', true);
+ * 
+ */
+
+$debug_hosts = array(
+'x10.0.0.1' => 1,
+'x192.168.0.1' => 1
+);
+
+/**
+ * Directory paths
+ */
+
+define('ONXSHOP_DIR', realpath(dirname(__FILE__) . '/../onyx/') . '/');
+define('ONXSHOP_PROJECT_DIR', realpath(dirname(__FILE__) . '/../') . '/');
+
+/**
+ * Database connection
+ */
+
+define('ONXSHOP_DB_TYPE', 'pgsql');
+define('ONXSHOP_DB_USER', getenv('ONYX_DB_USER'));
+define('ONXSHOP_DB_PASSWORD', getenv('ONYX_DB_PASSWORD'));
+define('ONXSHOP_DB_HOST', getenv('ONYX_DB_HOST'));
+define('ONXSHOP_DB_PORT', 5432);
+define('ONXSHOP_DB_NAME', getenv('ONYX_DB_NAME'));
+
+/**
+ * Salt used for encryption/hashing
+ */
+
+define('ONXSHOP_ENCRYPTION_SALT', '');
 
 /**
  * enable ecommerce
