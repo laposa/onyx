f79620c64259055e8669e7ff7f268349e4e3e21d
Patrik Szkandera <patrik@laposa.com>
2020-12-03 11:51:56 +0100

    remove Zend_Locale & Zend_Currency

--- controllers/locales.php
+++ controllers/locales.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2009-2013 Laposa Limited (https://laposa.ie)
+ * Copyright (c) 2009-2020 Laposa Limited (https://laposa.ie)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  */
 
@@ -9,94 +9,62 @@ class Onyx_Controller_Locales extends Onyx_Controller {
     /**
      * main action
      */
-     
-    public function mainAction() {
-    
-        /**
-         * Get input variables
-         */
+    public function mainAction()
+    {
+        // Get input variables
         if (preg_match("/\.bo\/backoffice\./", $this->GET['request'])) {
-        
             $locale = $GLOBALS['onyx_conf']['global']['locale'];
-        
         } else {
-        
             if ($_POST['locale']) {
-            
-                $locale_map = array('', 'en_GB.UTF-8', 'en_US.UTF-8', 'en_IE.UTF-8', 'cs_CZ.UTF-8', 'de_DE.UTF-8', 'en_AU.UTF-8', 'ja_JP.UTF-8', 'en_CA.UTF-8', 'en_HK.UTF-8', 'en_NZ.UTF-8', 'ru_RU.UTF-8', 'he_IL.UTF-8');
+                $locale_map = ['', 'en_GB.UTF-8', 'en_US.UTF-8', 'en_IE.UTF-8', 'cs_CZ.UTF-8', 'de_DE.UTF-8', 'en_AU.UTF-8', 'ja_JP.UTF-8', 'en_CA.UTF-8', 'en_HK.UTF-8', 'en_NZ.UTF-8', 'ru_RU.UTF-8', 'he_IL.UTF-8'];
                 $locale = $locale_map[$_POST['locale']];
-            
-            } else if ($_SESSION['locale']) {
-            
+            } elseif ($_SESSION['locale']) {
                 $locale = $_SESSION['locale'];
-            
             } else {
-            
                 if ($GLOBALS['onyx_conf']['global']['locale'] != '') $locale = $GLOBALS['onyx_conf']['global']['locale'];
                 else $locale = 'en_GB.UTF-8';
-            
             }
         }
 
-        
-        /**
-         * Check input variables
-         */
-        
-        
-        $allowed_locales = array('en_GB.UTF-8', 'en_US.UTF-8', 'en_IE.UTF-8', 'cs_CZ.UTF-8', 'de_DE.UTF-8', 'en_AU.UTF-8', 'ja_JP.UTF-8', 'en_CA.UTF-8', 'en_HK.UTF-8', 'en_NZ.UTF-8', 'ru_RU.UTF-8', 'he_IL.UTF-8');
-        
+        // Check input variables
+        $allowed_locales = ['en_GB.UTF-8', 'en_US.UTF-8', 'en_IE.UTF-8', 'cs_CZ.UTF-8', 'de_DE.UTF-8', 'en_AU.UTF-8', 'ja_JP.UTF-8', 'en_CA.UTF-8', 'en_HK.UTF-8', 'en_NZ.UTF-8', 'ru_RU.UTF-8', 'he_IL.UTF-8'];
+
         if (!in_array($locale, $allowed_locales)) {
-        
-            msg ("Invalid Locale", "error", 1);
-            
+            msg("Invalid Locale", "error", 1);
             if ($GLOBALS['onyx_conf']['global']['locale'] != '') $locale = $GLOBALS['onyx_conf']['global']['locale'];
             else $locale = 'en_GB.UTF-8';
-        
         }
-        
-        /**
-         * store across app and in session if different
-         */
-        
+
+        // store across app and in session if different
         define('LOCALE', $locale);
         define('LOCALE_OPENGRAPH', $this->mapToFacebook($locale));
-        
+
         if ($_SESSION['locale'] != LOCALE) $_SESSION['locale'] = LOCALE;
-        
-        /**
-         * Process
-         */
-        
         $this->setLocale($locale);
-        
 
         return true;
     }
-    
+
     /**
      * Set the locale
+     * @param mixed|string $locale
      */
-    
-    function setLocale($locale = LOCALE) {
-        
+    function setLocale($locale = LOCALE)
+    {
         //detect if local file with language string constants is available and load it
         $local_constants_file = ONYX_PROJECT_DIR . "locales/$locale/constants.php";
         if (file_exists($local_constants_file)) require_once($local_constants_file);
-        
+
         //load global language string constants file with fallback to en_GB
         $global_constants_file = ONYX_DIR . "locales/$locale/constants.php";
         if (!file_exists($global_constants_file)) $global_constants_file = ONYX_DIR . "locales/en_GB.UTF-8/constants.php";
         require_once($global_constants_file);
-        
+
         //now set system locale
         setlocale(LC_ALL, LOCALE);
         //but for numbers keep english
         setlocale(LC_NUMERIC, 'en_GB.UTF-8');
 
-        require_once('lib/Zend/Locale.php');
-        require_once('lib/Zend/Currency.php');
-        
         if (LOCALE == 'cs_CZ.UTF-8') {
             putenv("TZ=Europe/Prague");
             putenv("LANG=cs_CZ.UTF-8");
@@ -106,29 +74,26 @@ class Onyx_Controller_Locales extends Onyx_Controller {
             putenv("LANG=en_GB.UTF-8");
             date_default_timezone_set("Europe/London");
         }
-        
-        
-        $Zend_locale = new Zend_Locale(substr($locale, 0, 5));
-        
-        $Zend_currency = new Zend_Currency($Zend_locale->toString());
-        define('GLOBAL_LOCALE_CURRENCY', $Zend_currency->getShortName());
+
+        $currency = (new NumberFormatter(substr($locale, 0, 5), NumberFormatter::CURRENCY))
+            ->getTextAttribute(NumberFormatter::CURRENCY_CODE);
+        define('GLOBAL_LOCALE_CURRENCY', $currency);
     }
-    
+
     /**
      * mapToFacebook
      */
-     
-    public function mapToFacebook($locale) {
-        
+    public function mapToFacebook($locale)
+    {
         switch ($locale) {
             case 'en_IE.UTF-8';
                 $facebook_locale = 'en_GB';
-            break;
+                break;
             default:
                 $facebook_locale = substr($locale, 0, 5);
-            break;
+                break;
         }
-        
+
         return $facebook_locale;
     }
 }
--- models/education/education_survey_question_answer.php
+++ models/education/education_survey_question_answer.php
@@ -5,43 +5,41 @@
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  *
  */
- 
+
 class education_survey_question_answer extends Onyx_Model {
 
     /**
      * PRIMARY KEY
-     *
      */
     public $id;
-    
+
     /**
      * question_id
      */
     public $question_id;
-    
+
     /**
      * title
      */
     public $title;
-    
+
     /**
      * description
      * can use ### for substitution of input field
      */
     public $description;
-    
+
     /**
      * is_correct
      */
     public $is_correct;
-    
+
     /**
      * points
      * for test valuation
-     * 
      */
     public $points;
-    
+
     /**
      * priority
      */
@@ -60,25 +58,24 @@ class education_survey_question_answer extends Onyx_Model {
     /**
      * hashMap
      */
-     
-    public $_metaData = array(
-        'id'=>array('label' => '', 'validation'=>'int', 'required'=>true),
-        'question_id'=>array('label' => '', 'validation'=>'int', 'required'=>true),
-        'title'=>array('label' => '', 'validation'=>'string', 'required'=>true),
-        'description'=>array('label' => '', 'validation'=>'string', 'required'=>false),
-        'is_correct'=>array('label' => '', 'validation'=>'int', 'required'=>false),
-        'points'=>array('label' => '', 'validation'=>'int', 'required'=>true),
-        'priority'=>array('label' => '', 'validation'=>'int', 'required'=>false),
-        'publish'=>array('label' => '', 'validation'=>'int', 'required'=>false),
-        'other_data'=>array('label' => '', 'validation'=>'string', 'required'=>false)
-    );
-    
+
+    public $_metaData = [
+        'id'          => ['label' => '', 'validation' => 'int', 'required' => true],
+        'question_id' => ['label' => '', 'validation' => 'int', 'required' => true],
+        'title'       => ['label' => '', 'validation' => 'string', 'required' => true],
+        'description' => ['label' => '', 'validation' => 'string', 'required' => false],
+        'is_correct'  => ['label' => '', 'validation' => 'int', 'required' => false],
+        'points'      => ['label' => '', 'validation' => 'int', 'required' => true],
+        'priority'    => ['label' => '', 'validation' => 'int', 'required' => false],
+        'publish'     => ['label' => '', 'validation' => 'int', 'required' => false],
+        'other_data'  => ['label' => '', 'validation' => 'string', 'required' => false],
+    ];
+
     /**
      * create table sql
      */
-     
-    private function getCreateTableSql() {
-    
+    private function getCreateTableSql()
+    {
         $sql = "CREATE TABLE education_survey_question_answer (
             id serial PRIMARY KEY NOT NULL,
             question_id int NOT NULL REFERENCES education_survey_question ON UPDATE CASCADE ON DELETE CASCADE,
@@ -93,58 +90,45 @@ class education_survey_question_answer extends Onyx_Model {
 
             CREATE INDEX education_survey_question_answer_question_id_idx ON education_survey_question_answer (question_id);
         ";
-        
+
         return $sql;
     }
 
     /**
      * getDetail
      */
-     
-    public function getDetail($answer_id) {
-        
+    public function getDetail($answer_id)
+    {
         if (!is_numeric($answer_id)) {
             msg("Answer ID is not numeric", 'error');
             return false;
         }
-        
-        $detail = $this->detail($answer_id);
-        
-        return $detail;
+
+        return $this->detail($answer_id);
     }
-    
+
     /**
      * listAnswersForQuestion
      */
-     
-    public function listAnswersForQuestion($question_id) {
-    
+    public function listAnswersForQuestion($question_id)
+    {
         if (!is_numeric($question_id)) {
             msg("Question ID is not numeric", 'error');
             return false;
         }
-        
+
         $answer_list = $this->listing("question_id = $question_id", 'priority DESC, id ASC');
-        
         return $answer_list;
-        
     }
-     
+
     /**
      * saveAnswer
      */
-    
-    public function saveAnswer($data) {
-    
+    public function saveAnswer($data)
+    {
         if (!is_array($data)) return false;
-        //temp before switching to ZendDb in version 1.5
         if ($data['points'] == '') $data['points'] = 0;
-        /*require_once('lib/Zend/Db/Expr.php');
-        if ($data['points'] == '') $data['points'] = new Zend_Db_Expr('NULL');
-        msg(print_r($data, true));*/
-        
+
         return $this->save($data);
-        
     }
-    
 }
