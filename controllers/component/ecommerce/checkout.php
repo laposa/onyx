07f5c67a1bbb15bad2a817db2cd62047ee605e47
Patrik Szkandera <patrik@laposa.ie>
2021-02-05 11:21:55 +0100

    spam protection for checkout

--- controllers/component/ecommerce/checkout.php
+++ controllers/component/ecommerce/checkout.php
@@ -4,70 +4,70 @@
  *
  * Copyright (c) 2005-2011 Laposa Limited (https://laposa.ie)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
- * 
+ *
  */
 
 class Onyx_Controller_Component_Ecommerce_Checkout extends Onyx_Controller {
+    protected $enableReCaptcha;
 
     /**
      * main action
      */
-     
     public function mainAction() {
-    
-        /**
-         * get input
-         */
-         
+        $this->enableReCaptcha = ONYX_RECAPTCHA_PUBLIC_KEY && ONYX_RECAPTCHA_PRIVATE_KEY;
+        if ($this->enableReCaptcha) {
+            $this->tpl->parse("content.recaptcha_field");
+        }
+
+        // get input
         if (is_array($_POST['order'])) $order_data = $_POST['order'];
         else $order_data = array();
-        
-        /**
-         * get node configuration
-         */
-         
+
+        // get node configuration
         require_once('models/common/common_node.php');
         $node_conf = common_node::initConfiguration();
         $this->tpl->assign('NODE_CONF', $node_conf);
-        
-        /**
-         * init basket
-         */
-         
+
+        // init basket
         require_once('models/ecommerce/ecommerce_order.php');
         require_once('models/ecommerce/ecommerce_basket.php');
         $Order = new ecommerce_order();
         $Basket = new ecommerce_basket();
-        
+
         $Order->setCacheable(false);
         $Basket->setCacheable(false);
-        
+
         //temp
         if ($_POST['client']['customer']['currency_code']) {
             $currency_code = $_POST['client']['customer']['currency_code'];
         } else {
             $currency_code = $_SESSION['client']['customer']['currency_code'];
         }
-                
+
         if ($_SESSION['client']['customer']['id'] > 0) {
-            
             if (is_numeric($basket_id = $_SESSION['basket']['id'])) {
-            
                 //update basket
                 $basket_detail = $Basket->detail($basket_id);
                 $basket_detail['customer_id'] = $_SESSION['client']['customer']['id'];
                 $Basket->update($basket_detail);
-            
+
                 //insert order
-                if (isset($_POST['confirm'])) { 
-        
+                if (isset($_POST['confirm'])) {
+
+                    // verify captcha
+                    if ($this->enableReCaptcha) {
+                        $isCaptchaValid = verifyReCaptchaToken($_POST['g-recaptcha-response']);
+                        if (!$isCaptchaValid) {
+                            msg('Invalid security code', 'error');
+                            return false;
+                        }
+                    }
+
                     if ($_POST['order_terms_agreed'] == 'on') {
-                    
                         //insert only orders with some items in the basket :)
                         $basket_content = $Basket->getFullDetail($basket_id);
-                    
+
                         if (count($basket_content['items']) > 0) {
-                        
                             $order_data = $_POST['order'];
 
                             $order_data['basket_id'] = $_SESSION['basket']['id'];
@@ -76,12 +76,11 @@ class Onyx_Controller_Component_Ecommerce_Checkout extends Onyx_Controller {
                             $order_data['other_data']['delivery_options'] = $_SESSION['delivery_options'];
                             $order_data['other_data']['promotion_code'] = $_SESSION['promotion_code'];
                             $order_data['php_session_id'] = session_id();
-                            
+
                             if ($inserted_order_id = $Order->insertOrder($order_data)) {
-                                
                                 $_SESSION['promotion_code'] = null;
                                 $_SESSION['basket']['id'] = null;
-                                
+
                                 //forward to payment page with pre-selected payment method
                                 //onyxGoTo("page/" . $node_conf['id_map-payment'] . "?order_id=$inserted_order_id&selected_poyment_type={$order_data['payment_type']}");
                                 onyxGoTo("page/" . $node_conf['id_map-payment'] . "?order_id=$inserted_order_id");
@@ -89,127 +88,91 @@ class Onyx_Controller_Component_Ecommerce_Checkout extends Onyx_Controller {
                         } else {
                             msg("Can't insert an empty order.", 'error');
                         }
-                        
                     } else {
-                        
                         msg("You must agree with our Terms & Conditions", 'error');
                     }
                 }
             }
-        
-            /**
-             * prepare list of payment options
-             */
-             
+
+            // prepare list of payment options
             require_once('models/ecommerce/ecommerce_transaction.php');
             $Transaction = new ecommerce_transaction();
             $transaction_type_allowed = $Transaction->conf['allowed_types'];
             foreach ($transaction_type_allowed as $type) {
                 $this->tpl->parse("content.$type");
             }
-        
-            /**
-             * gift option
-             */
 
+            // gift option
             if ($order_data['other_data']['gift'] == 1) {
                 $this->tpl->assign("CHECKED_order-other_data-gift", "checked='checked'");
                 $this->tpl->assign('GIFT_DISPLAY', 'block');
             } else {
                 $this->tpl->assign('GIFT_DISPLAY', 'none');
             }
-            
         } else {
             //msg('You must be logged in first.', 'error');
             $_SESSION['to'] = "page/" . $node_conf['id_map-checkout'];
             onyxGoTo("page/" . $node_conf['id_map-login']);
         }
-        
-        $this->tpl->assign("ORDER", $_POST['order']);
 
+        $this->tpl->assign("ORDER", $_POST['order']);
         return true;
     }
-    
+
     /**
      * check for virtual product only
      */
-     
     public function isBasketVirtualProductOnly() {
-        
         if ($gift_voucher_product_id = $this->getGiftVoucherProductId()) {
-        
             if (is_numeric($basket_id = $_SESSION['basket']['id'])) {
-                
                 require_once('models/ecommerce/ecommerce_basket.php');
                 $Basket = new ecommerce_basket();
-                
                 $basket_content = $Basket->getFullDetail($basket_id);
-                
                 $voucher_basket_items = $this->getVoucherBasketItems($basket_content['items'], $gift_voucher_product_id);
-                
+
                 if (!$voucher_basket_items) return false;
                 if (count($voucher_basket_items) == count($basket_content['items'])) return true;
                 return false;
-                
             } else {
-            
                 return false;
-            
             }
-
         } else {
-        
             return false;
-        
         }
     }
-    
+
     /**
      * getGiftVoucherProductId
      */
-     
     public function getGiftVoucherProductId() {
-        
-        /**
-         * get product conf
-         */
-         
+        // get product conf
         require_once('models/ecommerce/ecommerce_product.php');
         $ecommerce_product_conf = ecommerce_product::initConfiguration();
-        
-        /**
-         * check gift voucher product ID is set
-         */
-         
+
+        // check gift voucher product ID is set
         if (!is_numeric($ecommerce_product_conf['gift_voucher_product_id']) || $ecommerce_product_conf['gift_voucher_product_id']  == 0) {
-            
             return false;
         }
-        
+
         return $ecommerce_product_conf['gift_voucher_product_id'];
     }
-    
+
     /**
      * getVoucherBasketItems
      */
-     
     public function getVoucherBasketItems($basket_items, $gift_voucher_product_id) {
-        
         if (!is_array($basket_items)) return false;
         if (!is_numeric($gift_voucher_product_id)) return false;
-        
+
         $voucher_basket_items = array();
-        
         foreach ($basket_items as $basket_item) {
-            
             if ($basket_item['product']['id'] == $gift_voucher_product_id) {
                 $voucher_basket_items[] = $basket_item;
             }
-            
+
         }
-        
+
         if (count($voucher_basket_items) > 0) return $voucher_basket_items;
         else return false;
-        
     }
 }
