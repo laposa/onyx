a16c9f72df45a109f32c75315bfb9ee6704ac1ca
Norbert Laposa <norbert@laposa.ie>
2021-03-15 17:14:53 +0000

    fix PHP7 warnings

--- lib/onyx.functions.php
+++ lib/onyx.functions.php
@@ -426,7 +426,7 @@ function php_multisort($data,$keys){
     $i=0;
     foreach ($keys as $k){
         if($i>0){$sort.=',';}
-        $sort.='$cols['.$k['key'].']';
+        $sort.="\$cols['".$k['key']."']";
         if($k['sort']){$sort.=',SORT_'.strtoupper($k['sort']);}
         if($k['type']){$sort.=',SORT_'.strtoupper($k['type']);}
         $i++;
