d40d32538ab0ed507bd16b534c4e519ec97b639f
Norbert Laposa <norbert@laposa.ie>
2021-09-08 16:14:25 +0100

    upgrade jQuery to v3.6.0

--- utils/compile_js_common.sh
+++ utils/compile_js_common.sh
@@ -10,12 +10,9 @@ cat ${FULL_PATH}/share/js/jquery/jquery.js >> ${COMPILED_JS} && echo "" >> ${COM
 cat ${FULL_PATH}/share/js/jquery/jquery-migrate.js >> ${COMPILED_JS}  && echo "" >> ${COMPILED_JS}
 cat ${FULL_PATH}/share/js/jquery/plugins/jquery.tools.js >> ${COMPILED_JS} && echo "" >> ${COMPILED_JS}
 cat ${FULL_PATH}/share/js/jquery/plugins/jquery.easing.js >> ${COMPILED_JS} && echo "" >> ${COMPILED_JS}
-cat ${FULL_PATH}/share/js/jquery/plugins/jquery.form.js >> ${COMPILED_JS} && echo "" >> ${COMPILED_JS}
 cat ${FULL_PATH}/share/js/jquery/plugins/jquery.jgrowl.js >> ${COMPILED_JS} && echo "" >> ${COMPILED_JS}
 cat ${FULL_PATH}/share/js/jquery/plugins/jquery.validate.js >> ${COMPILED_JS} && echo "" >> ${COMPILED_JS}
 cat ${FULL_PATH}/share/js/jquery/plugins/reflection.js >> ${COMPILED_JS} && echo "" >> ${COMPILED_JS}
 cat ${FULL_PATH}/share/js/jquery/plugins/jquery.form.js >> ${COMPILED_JS} && echo "" >> ${COMPILED_JS}
 cat ${FULL_PATH}/share/js/jquery/plugins/jquery.mousewheel.js >> ${COMPILED_JS} && echo "" >> ${COMPILED_JS}
-#cat ${FULL_PATH}/share/js/jquery/plugins/jquery.fancybox.js >> ${COMPILED_JS} && echo "" >> ${COMPILED_JS}
-cat ${FULL_PATH}/share/js/js.cookie.js >> ${COMPILED_JS} && echo "" >> ${COMPILED_JS}
 cat ${FULL_PATH}/share/js/common.js >> ${COMPILED_JS} && echo "" >> ${COMPILED_JS}
