a819a398c0536b44158cfb858fd59f19f23dd67b
Petr Hugo Dvorak <hugo@laposa.ie>
2024-10-17 16:41:01 +0200

    remove dependency on vlucas/phpdotenv from skeleton

--- project_skeleton/base/conf/global.php
+++ project_skeleton/base/conf/global.php
@@ -17,15 +17,6 @@
 require_once(__DIR__ . '/../vendor/autoload.php');
 
 /**
- * Load env variables
- */
-
-if (file_exists(__DIR__ . "/../.env")) {
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
-    $dotenv->load();
-}
-
-/**
  * Directory paths
  */
 
