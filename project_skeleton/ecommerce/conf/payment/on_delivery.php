9240e7f76952d9b230abf7faf442078cf195200b
Norbert Laposa <norbert@laposa.ie>
2025-04-14 11:18:54 +0100

    move project_skeleton to separate repo

--- project_skeleton/ecommerce/conf/payment/on_delivery.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * on delivery payment configuration
- *
- * Copyright (c) 2012 Laposa Limited (https://laposa.ie)
- * Licensed under the New BSD License. See the file LICENSE.txt for details.
- *
- */
-
