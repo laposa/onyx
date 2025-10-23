966bfada3df1280c527cbaa9c7326f7188dbe814
Norbert Laposa <norbert@laposa.ie>
2025-10-23 16:13:49 +0100

    make sure properties are defined

--- controllers/component/menu_stack.php
+++ controllers/component/menu_stack.php
@@ -1,6 +1,6 @@
 <?php
 /** 
- * Copyright (c) 2015 Laposa Limited (https://laposa.ie)
+ * Copyright (c) 2015-2025 Laposa Limited (https://laposa.ie)
  * Licensed under the New BSD License. See the file LICENSE.txt for details.
  *
  */
@@ -15,9 +15,12 @@ class Onyx_Controller_Component_Menu_Stack extends Onyx_Controller_Component_Men
      
     public function parseItem($item)
     {
-        $image_width = $this->GET['image_width'];
-        $image_height = $this->GET['image_height'];
-        $image_fill = $this->GET['image_fill'];
+        if (array_key_exists('image_width', $this->GET)) $image_width = $this->GET['image_width'];
+        else $image_width = null;
+        if (array_key_exists('image_height', $this->GET)) $image_height = $this->GET['image_height'];
+        else $image_height = null;
+        if (array_key_exists('image_fill', $this->GET)) $image_fill = $this->GET['image_fill'];
+        else $image_fill = null;
         
         $_Onyx_Request = new Onyx_Request("component/teaser_stack~target_node_id={$item['id']}:image_width=$image_width:image_height=$image_height:image_fill=$image_fill~");
         $item['teaser_content'] = $_Onyx_Request->getContent();
