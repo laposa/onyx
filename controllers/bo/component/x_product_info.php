f1643835acd3a7e39c24c57c62931cb7a059b10c
Daniel Kuchař <daniel@laposa.ie>
2025-12-05 12:26:15 +0100

    fix dolphin pools related issues

--- controllers/bo/component/x_product_info.php
+++ controllers/bo/component/x_product_info.php
@@ -7,6 +7,7 @@
 
 require_once('controllers/bo/component/x.php');
 require_once('models/ecommerce/ecommerce_product.php');
+require_once('models/common/common_node.php');
 
 class Onyx_Controller_Bo_Component_X_Product_Info extends Onyx_Controller_Bo_Component_X {
 
@@ -16,10 +17,14 @@ class Onyx_Controller_Bo_Component_X_Product_Info extends Onyx_Controller_Bo_Com
      
     public function mainAction() {
 
-        // get details
-        $product = new ecommerce_product();
-        $product_data = $product->productDetail($this->GET['product_id']);
+        // node details
+        $node = new common_node();
+        $node_data = $node->detail($this->GET['node_id']);
 
+        // get product details from node content in order to avoid issues with refreshing
+        $product = new ecommerce_product();
+        $product_data = $product->productDetail(is_numeric($node_data['content']) ? $node_data['content'] : null);
+        
         // save
         if (isset($_POST['save'])) {
             // TODO: messages
@@ -30,6 +35,8 @@ class Onyx_Controller_Bo_Component_X_Product_Info extends Onyx_Controller_Bo_Com
                 msg("Cannot update node {$product_data['name']} (id={$product_data['id']})", 'error');
             }
         }
+        
+        $this->tpl->assign('NODE', $node_data);
 
         if ($product_data) {
             $this->tpl->assign('PRODUCT', $product_data);
