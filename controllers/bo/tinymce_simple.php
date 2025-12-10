eea4ed8135211895add2a1c0f0f20cb18c94137c
Daniel Kuchař <daniel@laposa.ie>
2025-12-10 11:41:27 +0100

    upgrade tinymce, replace old tinymce with hugerte

---
+++ controllers/bo/tinymce_simple.php
@@ -0,0 +1,19 @@
+<?php
+/** 
+ * Copyright (c) 2006-2011 Laposa Limited (https://laposa.ie)
+ * Licensed under the New BSD License. See the file LICENSE.txt for details.
+ *
+ */
+
+class Onyx_Controller_Bo_Tinymce_Simple extends Onyx_Controller {
+
+    /**
+     * main action
+     */
+     
+    public function mainAction() {
+        $tinymce['edit_elements'] = isset($this->GET['edit_elements']) ? ($this->GET['edit_elements'] == '' ? 'edit-content' : $this->GET['edit_elements']) : '';
+        $this->tpl->assign('TINYMCE', $tinymce);
+        return true;
+    }
+}
