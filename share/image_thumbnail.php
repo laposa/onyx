db5c3d15b916cde226b3afd1fcc6f3cbb2a5da4f
Petr Hugo Dvorak <hugo@laposa.ie>
2026-05-29 18:16:05 +0200

    update mime type images

--- share/image_thumbnail.php
+++ share/image_thumbnail.php
@@ -92,9 +92,8 @@ if ($width > $image_configuration['width_max']) {
          * check what to display
          */
          
-        $scale = 0.3;
         if (preg_match("/image/", $mime_type)) {
-            $scale = 1;
+            // nothing, just resize
         } else if ($mime_type == 'application/pdf') {
             $image_file = "public_html/share/images/mimetype/pdf.png";  
         } else if ($mime_type == 'application/msword') {
@@ -109,7 +108,7 @@ if ($width > $image_configuration['width_max']) {
          * try
          */
 
-        if ($thumbnail = common_image::resize($image_file, $width, $height, $method, $gravity, $fill, $scale)) $image_file = $thumbnail;
+        if ($thumbnail = common_image::resize($image_file, $width, $height, $method, $gravity, $fill)) $image_file = $thumbnail;
         
     } else {
 
